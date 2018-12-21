c05e54ea968cf7800c12ed5692dece437bc1f7c2
Florin Alexandru Marin <Florin-Alexandru.Marin@24software.ro>
2018-12-21 15:19:27 +0000

    date inputs allow typing - changes to work for all input types | Betty
    Workitems:
    #9482 UI/UX - General-Common-Need the date field editable
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C17711

--- ShipTech10/app/app-masters/controllers.js
+++ ShipTech10/app/app-masters/controllers.js
@@ -6413,58 +6413,97 @@
             return null;
         }
 
-        vm.setValue = function(name, direction, simpleDate, app){
-            // debugger;
+        vm.setValue = function(inputDetails, direction, simpleDate, app){
+            
 
+            /**  @param inputDetails - object w. the inputs details:
+             * 
+             *    - root: CM/$scope/$rootScope/any other controller
+             *    - path: string path to the object, can be nested: "formValues.deliveryProducts[0].headers.myValue"
+             *    - isNested: true if path is nested, false otherwise
+             *    - pickerId: id set to <span class="input-group-btn date-picker">, used to set date inside the datepicker after date is set manually
+             * 
+             *    Note: deep nested properties will be accessed using lodash
+             *    _.get(root, path) _.get(object, 'a[0].b.c');
+             *    _.set(root, path, valueToSet); // _.set(object, 'a[0].b.c', 4);
+             * 
+             *   @param direction - number (1 / 2)
+             *    - whether function is called after datepicker changes date (1) or after date is changed by typing (2)
+             * 
+             *   @param simpleDate - boolean 
+             *    - true if date is date-only
+             * 
+             *   @param app - string
+             *    - sometimes this is needed when calling vm.formatSimpleDate
+             * 
+             *   ------------------------------------------------------------------------------
+             * 
+             *  1. Date formed by typing is stored in root.formatDates[path]
+                So if i have Some_Controller.nested.nested2.value => the formated value will be in Some_Controller.formatDates.nested.nested2.value 
 
-			if(typeof name == "object") {
-	            modelElementScope = angular.element($(event.target).parent()).scope();
-	            KBmodifiedDateVal = $(event.target).val();
-				
-			}
+                2. @variable DATE_FORMAT is set to $scope.tenantSetting.tenantFormats.dateFormat
+                  This needs to be changed if it has other path in other controller
+             */
 
+            var DATE_FORMAT = $scope.tenantSetting.tenantFormats.dateFormat;
+
+            var rootMap = {
+                '$scope': $scope,
+                '$rootScope': $rootScope,
+                'vm': vm
+            }
 
             if(direction == 1){
                 // datepicker input -> date typing input
                 $timeout(function() {
                     if(simpleDate){
-                        $scope.formatDates[name] = vm.formatSimpleDate(eval('modelElementScope.'+name.path),$scope.tenantSetting.tenantFormats.dateFormat, app)
+                        var dateValue = _.get(rootMap[inputDetails.root],inputDetails.path);
+                        var formattedDate = vm.formatSimpleDate(dateValue, DATE_FORMAT, app);
+                        _.set(rootMap[inputDetails.root], "formatDates." + inputDetails.path, formattedDate); 
                     } else{
-
-                        $scope.formatDates[name] = vm.formatDateTime(eval('modelElementScope.'+name.path), $scope.tenantSetting.tenantFormats.dateFormat);
+                        var dateValue = _.get(rootMap[inputDetails.root],inputDetails.path);
+                        var formattedDate = vm.formatDateTime(dateValue, DATE_FORMAT);
+                        _.set(rootMap[inputDetails.root], "formatDates." + inputDetails.path, formattedDate); 
                     }
-                    // console.log( $scope.formatDates[name], $scope.formValues[name]);
                 },2);
             }
             if(direction == 2){
                 // date typing input -> datepicker input 
                 $timeout(function() { 
-                    // every changes goes here
+                    
+                    var date = _.get(rootMap[inputDetails.root], "formatDates." +  inputDetails.path);
+                    var copy = angular.copy(date);
+                    var formattedDate = vm.formatDateTimeReverse(copy, simpleDate);
+                    _.set(rootMap[inputDetails.root], inputDetails.path, formattedDate); 
 
-                    if(typeof name == "string"){
-                        // only one level
-                        var copy = angular.copy($scope.formatDates[name]);
-                        $scope.formValues[name] = vm.formatDateTimeReverse(copy, simpleDate);
-                       // set date for datepicker
-                       $('.date-picker#' + name).datetimepicker('setDate', new Date($scope.formValues[name]));
-                    }else if(typeof name == "object"){
-                        if(name.type == "eval"){
-                            // var value = eval("$scope.formatDates." + name.path);
-                            var date = vm.formatDateTimeReverse(KBmodifiedDateVal, simpleDate);
-// function index(modelElementScope,i) {return modelElementScope[i]}
-// dateObjectModel = name.path.split('.').reduce(index, modelElementScope)
-// dateObjectModel = eval(modelElementScope + "." + name.path)
+                    // also change datepicker value
+                    $('.date-picker#' + inputDetails.pickerId).datetimepicker('setDate', new Date(formattedDate));
 
-// dateObjectModel = _.get(modelElementScope, name.path);
-                            // set date for datepicker
-                            $('.date-picker#' + name.pickerId).datetimepicker('setDate', new Date(date));
-                            // modelElementScope.$apply(function(){
-                            // })
-	                            eval('modelElementScope.'+name.path +' = '+ '"'+date+'"');
-                            // $parse("modelElementScope." + name.path).assign(date);
 
-                        }
-                    }
+                    // if(typeof name == "string"){
+                    //     // only one level
+                    //     var copy = angular.copy($scope.formatDates[name]);
+                    //     $scope.formValues[name] = vm.formatDateTimeReverse(copy, simpleDate);
+                    //    // set date for datepicker
+                    //    $('.date-picker#' + name).datetimepicker('setDate', new Date($scope.formValues[name]));
+                    // }else if(typeof name == "object"){
+                    //     if(name.type == "eval"){
+                    //         // var value = eval("$scope.formatDates." + name.path);
+                    //         var date = vm.formatDateTimeReverse(KBmodifiedDateVal, simpleDate);
+                    //         // function index(modelElementScope,i) {return modelElementScope[i]}
+                    //         // dateObjectModel = name.path.split('.').reduce(index, modelElementScope)
+                    //         // dateObjectModel = eval(modelElementScope + "." + name.path)
+
+                    //         // dateObjectModel = _.get(modelElementScope, name.path);
+                    //         // set date for datepicker
+
+                    //         // modelElementScope.$apply(function(){
+                    //         // })
+	                //             eval('modelElementScope.'+name.path +' = '+ '"'+date+'"');
+                    //         // $parse("modelElementScope." + name.path).assign(date);
+
+                    //     }
+                    // }
                 },2);
             }
         }
