3e56618091f32f62066ca0718d42d2aae2b79cfb
Alexandru.boasca@24software.ro <Alexandru.boasca@24software.ro>
2019-02-25 18:54:34 +0000

    Hide table datepicker on treasury report when clicking date to or for
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C19160

--- ShipTech10/app/app-general-components/directives.js
+++ ShipTech10/app/app-general-components/directives.js
@@ -619,6 +619,18 @@ window.increment = 0;
                                             }
                                         });
                                         $('select[name="asc_jqgrid__entries-entries"] option[value="999999"]').text("All");
+                                        if (Layout.table_id == 'flat_invoices_app_complete_view_list') {
+                                            $('#dp_PaymentDateFrom .date-set').click(function() {
+                                                setTimeout(function() {
+                                                    $('.datepicker').remove();
+                                                }, 0);
+                                            });
+                                            $('#dp_PaymentDateTo .date-set').click(function() {
+                                                setTimeout(function() {
+                                                    $('.datepicker').remove();
+                                                }, 0);
+                                            });
+                                        }
                                     }
                                 }
                             );
