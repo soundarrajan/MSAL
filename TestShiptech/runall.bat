b6793c7b0e3803ecb6093cf80e943f48b046371a
rares.tohanean@24software.ro <rares.tohanean@24software.ro>
2019-03-29 13:24:19 +0000

    Add batch file to run all tests
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C19762

---
+++ TestShiptech/runall.bat
@@ -0,0 +1,5 @@
+node TestInvoice ./TestCases/Scenario_1.json
+node TestInvoice ./TestCases/Scenario_2.json
+node TestInvoice ./TestCases/Scenario_3.json
+node TestInvoice ./TestCases/Scenario_4.json
+node TestInvoice ./TestCases/Scenario_5.json
