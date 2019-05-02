21c3c064a21d34c49b222639eaf1e9f23373b330
Florin Alexandru Marin <Florin-Alexandru.Marin@24software.ro>
2019-05-02 11:24:41 +0000

    Fixed column widths on chrome zoom out
    Workitems:
    #13748 002 - UI / UX - Need to have scroll bar in grids to freeze grid headers
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C20386

--- ShipTech10/app/app-general-components/directives.js
+++ ShipTech10/app/app-general-components/directives.js
@@ -936,7 +936,34 @@ window.increment = 0;
                                             }
                                         }
                                         Elements.settings[settings_keys[i]].source.width = Elements.settings[settings_keys[i]].source.width - overWidth;
-                                        $(Elements.table[Elements.settings[settings_keys[i]].table]).jqGrid("setGridWidth", Elements.settings[settings_keys[i]].source.width, true);
+                                        
+                                        var screenCssPixelRatio = (window.outerWidth - 8) / window.innerWidth;
+										if (screenCssPixelRatio >= .46 && screenCssPixelRatio <= .54) {
+										  zoomLevel = "-4";
+										} else if (screenCssPixelRatio <= .64) {
+										  zoomLevel = "-3";
+										} else if (screenCssPixelRatio <= .76) {
+										  zoomLevel = "-2";
+										} else if (screenCssPixelRatio <= .92) {
+										  zoomLevel = "-1";
+										} else if (screenCssPixelRatio <= 1.10) {
+										  zoomLevel = "0";
+										} else if (screenCssPixelRatio <= 1.32) {
+										  zoomLevel = "1";
+										} else if (screenCssPixelRatio <= 1.58) {
+										  zoomLevel = "2";
+										} else if (screenCssPixelRatio <= 1.90) {
+										  zoomLevel = "3";
+										} else if (screenCssPixelRatio <= 2.28) {
+										  zoomLevel = "4";
+										} else if (screenCssPixelRatio <= 2.70) {
+										  zoomLevel = "5";
+										} else {
+										  zoomLevel = "unknown";
+										}
+										if (zoomLevel == "0") {
+	                                        $(Elements.table[Elements.settings[settings_keys[i]].table]).jqGrid("setGridWidth", Elements.settings[settings_keys[i]].source.width, true);
+										}
                                     }
                                     MCScustom.load();
                                 }
