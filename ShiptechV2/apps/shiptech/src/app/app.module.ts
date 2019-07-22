40bf9d8cd2ac92a1bf8cce9962bbf5c02e1c202e
Cusnir Dumitru <dumitru.cusnir@24software.ro>
2019-07-22 13:07:45 +0300

    Design v1

--- ShiptechV2/apps/shiptech/src/app/app.module.ts
+++ ShiptechV2/apps/shiptech/src/app/app.module.ts
@@ -2,11 +2,142 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppFooterComponent } from './components/app.footer.component';
+import { AppMainComponent } from './components/app.main.component';
+import { AppMenuComponent, AppSubMenuComponent } from './components/app.menu.component';
+import { AppTopBarComponent } from './components/app.topbar.component';
+import { AppRightMenuComponent } from './components/app.right-menu.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  AccordionModule, AutoCompleteModule,
+  BreadcrumbModule,
+  ButtonModule, CalendarModule,
+  CardModule, CarouselModule,
+  ChartModule, CheckboxModule,
+  ChipsModule, CodeHighlighterModule, ColorPickerModule,
+  ConfirmDialogModule, ContextMenuModule, DialogModule,
+  DropdownModule, EditorModule, FieldsetModule, FileUploadModule,
+  GalleriaModule,
+  GrowlModule,
+  InplaceModule, InputMaskModule,
+  InputSwitchModule,
+  InputTextareaModule, InputTextModule, LightboxModule,
+  ListboxModule,
+  MegaMenuModule, MenubarModule,
+  MenuModule, MessageModule,
+  MessagesModule,
+  MultiSelectModule, OrderListModule, OrganizationChartModule, OverlayPanelModule,
+  PaginatorModule,
+  PanelMenuModule,
+  PanelModule,
+  PasswordModule,
+  PickListModule, ProgressBarModule, RadioButtonModule, RatingModule,
+  SelectButtonModule, SlideMenuModule,
+  SliderModule, SpinnerModule, SplitButtonModule, StepsModule,
+  TabMenuModule, TabViewModule,
+  TerminalModule, TieredMenuModule, ToggleButtonModule, ToolbarModule, TooltipModule,
+  TreeModule,
+  TreeTableModule
+} from 'primeng/primeng';
+import { FullCalendarModule } from 'primeng/fullcalendar';
+import { ToastModule } from 'primeng/toast';
+import { TableModule } from 'primeng/table';
+import { DataViewModule } from 'primeng/dataview';
+import { VirtualScrollerModule } from 'primeng/virtualscroller';
+import { FormsModule } from '@angular/forms';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+export const PRIME_IMPORTS = [
+  AccordionModule,
+  AutoCompleteModule,
+  BreadcrumbModule,
+  ButtonModule,
+  CalendarModule,
+  CardModule,
+  CarouselModule,
+  ChartModule,
+  CheckboxModule,
+  ChipsModule,
+  CodeHighlighterModule,
+  ConfirmDialogModule,
+  ColorPickerModule,
+  ContextMenuModule,
+  DataViewModule,
+  DialogModule,
+  DropdownModule,
+  EditorModule,
+  FieldsetModule,
+  FileUploadModule,
+  FullCalendarModule,
+  GalleriaModule,
+  GrowlModule,
+  InplaceModule,
+  InputMaskModule,
+  InputSwitchModule,
+  InputTextModule,
+  InputTextareaModule,
+  LightboxModule,
+  ListboxModule,
+  MegaMenuModule,
+  MenuModule,
+  MenubarModule,
+  MessageModule,
+  MessagesModule,
+  MultiSelectModule,
+  OrderListModule,
+  OrganizationChartModule,
+  OverlayPanelModule,
+  PaginatorModule,
+  PanelModule,
+  PanelMenuModule,
+  PasswordModule,
+  PickListModule,
+  ProgressBarModule,
+  RadioButtonModule,
+  RatingModule,
+  // ScrollPanelModule,
+  SelectButtonModule,
+  SlideMenuModule,
+  SliderModule,
+  SpinnerModule,
+  SplitButtonModule,
+  StepsModule,
+  TableModule,
+  TabMenuModule,
+  TabViewModule,
+  TerminalModule,
+  TieredMenuModule,
+  ToastModule,
+  ToggleButtonModule,
+  ToolbarModule,
+  TooltipModule,
+  TreeModule,
+  TreeTableModule,
+  VirtualScrollerModule
+]
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    AppMainComponent,
+    AppMenuComponent,
+    AppRightMenuComponent,
+    AppSubMenuComponent,
+    AppTopBarComponent,
+    AppFooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([]),
+    FormsModule,
+    ScrollingModule,
+    ...PRIME_IMPORTS
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ ShiptechV2/apps/shiptech/src/app/components/app.main.component.ts
@@ -0,0 +1,192 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shiptech-main',
+    templateUrl: './app.main.component.html',
+})
+export class AppMainComponent {
+    menuMode = 'static';
+
+    overlayMenuActive: boolean;
+
+    staticMenuDesktopInactive: boolean;
+
+    staticMenuMobileActive: boolean;
+
+    layoutMenuScroller: HTMLDivElement;
+
+    lightMenu = true;
+
+    topbarColor = 'layout-topbar-light';
+
+    menuClick: boolean;
+
+    userMenuClick: boolean;
+
+    notificationMenuClick: boolean;
+
+    rightMenuClick: boolean;
+
+    resetMenu: boolean;
+
+    menuHoverActive: boolean;
+
+    topbarUserMenuActive: boolean;
+
+    topbarNotificationMenuActive: boolean;
+
+    rightPanelMenuActive: boolean;
+
+    inlineUser: boolean;
+
+    isRTL: boolean;
+
+    onLayoutClick() {
+        if (!this.userMenuClick) {
+            this.topbarUserMenuActive = false;
+        }
+
+        if (!this.notificationMenuClick) {
+            this.topbarNotificationMenuActive = false;
+        }
+
+        if (!this.rightMenuClick) {
+            this.rightPanelMenuActive = false;
+        }
+
+        if (!this.menuClick) {
+            if (this.isHorizontal() || this.isSlim()) {
+                this.resetMenu = true;
+            }
+
+            if (this.overlayMenuActive || this.staticMenuMobileActive) {
+                this.hideOverlayMenu();
+            }
+
+            this.menuHoverActive = false;
+            this.unblockBodyScroll();
+        }
+
+        this.userMenuClick = false;
+        this.rightMenuClick = false;
+        this.notificationMenuClick = false;
+        this.menuClick = false;
+    }
+
+    onMenuButtonClick(event) {
+        this.menuClick = true;
+        this.topbarUserMenuActive = false;
+        this.topbarNotificationMenuActive = false;
+        this.rightPanelMenuActive = false;
+
+        if (this.isOverlay()) {
+            this.overlayMenuActive = !this.overlayMenuActive;
+        }
+
+        if (this.isDesktop()) {
+            this.staticMenuDesktopInactive = !this.staticMenuDesktopInactive;
+        } else {
+            this.staticMenuMobileActive = !this.staticMenuMobileActive;
+            if (this.staticMenuMobileActive) {
+                this.blockBodyScroll();
+            } else {
+                this.unblockBodyScroll();
+            }
+        }
+
+        event.preventDefault();
+    }
+
+    onMenuClick($event) {
+        this.menuClick = true;
+        this.resetMenu = false;
+    }
+
+    onTopbarUserMenuButtonClick(event) {
+        this.userMenuClick = true;
+        this.topbarUserMenuActive = !this.topbarUserMenuActive;
+
+        this.hideOverlayMenu();
+
+        event.preventDefault();
+    }
+
+    onTopbarNotificationMenuButtonClick(event) {
+        this.notificationMenuClick = true;
+        this.topbarNotificationMenuActive = !this.topbarNotificationMenuActive;
+
+        this.hideOverlayMenu();
+
+        event.preventDefault();
+    }
+
+    onRightMenuClick(event) {
+        this.rightMenuClick = true;
+        this.rightPanelMenuActive = !this.rightPanelMenuActive;
+
+        this.hideOverlayMenu();
+
+        event.preventDefault();
+    }
+
+    onTopbarSubItemClick(event) {
+        event.preventDefault();
+    }
+
+    isHorizontal() {
+        return this.menuMode === 'horizontal';
+    }
+
+    isSlim() {
+        return this.menuMode === 'slim';
+    }
+
+    isOverlay() {
+        return this.menuMode === 'overlay';
+    }
+
+    isStatic() {
+        return this.menuMode === 'static';
+    }
+
+    isMobile() {
+        return window.innerWidth < 1025;
+    }
+
+    isDesktop() {
+        return window.innerWidth > 896;
+    }
+
+    isTablet() {
+        const width = window.innerWidth;
+        return width <= 1024 && width > 640;
+    }
+
+    hideOverlayMenu() {
+        this.overlayMenuActive = false;
+        this.staticMenuMobileActive = false;
+    }
+
+    changeMenuMode(menuMode: string) {
+        this.menuMode = menuMode;
+        this.staticMenuDesktopInactive = false;
+        this.overlayMenuActive = false;
+    }
+
+    blockBodyScroll(): void {
+        if (document.body.classList) {
+            document.body.classList.add('blocked-scroll');
+        } else {
+            document.body.className += ' blocked-scroll';
+        }
+    }
+
+    unblockBodyScroll(): void {
+        if (document.body.classList) {
+            document.body.classList.remove('blocked-scroll');
+        } else {
+            document.body.className = document.body.className.replace(new RegExp('(^|\\b)' +
+                'blocked-scroll'.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+    }
+}
