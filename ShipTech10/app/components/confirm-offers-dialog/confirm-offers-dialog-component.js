d90c8b8bd0b09a92ba4b7d7ec7be1643806416ab
Florin Alexandru Marin <Florin-Alexandru.Marin@24software.ro>
2019-07-30 15:34:37 +0000

    Open single order in the same tab
    Workitems:
    #14760 Negotiation - Need hyperlink to open the orders created for each request in new browser tab
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C22185

--- ShipTech10/app/components/confirm-offers-dialog/confirm-offers-dialog-component.js
+++ ShipTech10/app/components/confirm-offers-dialog/confirm-offers-dialog-component.js
@@ -239,7 +239,7 @@ angular.module('shiptech.components').controller('ConfirmOffersDialogController'
                            if (receivedOffers.length == 1) {
 	                           	$("a.closeBlade").click();
 	                           	$rootScope.$broadcast("initScreenAfterSendOrSkipRfq", true);                           	
-                                window.open('/#/edit-order/' + receivedOffers[0]);
+                                window.location.href = '/#/edit-order/' + receivedOffers[0];
                            } else {
 	                           	$("a.closeBlade").click();
 	                           	$rootScope.$broadcast("initScreenAfterSendOrSkipRfq", true);
