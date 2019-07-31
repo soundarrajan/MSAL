86cb21a56f5642b2c5185ef7178ec95682af4554
Cusnir Dumitru <dumitru.cusnir@24software.ro>
2019-07-31 15:10:00 +0300

    Fixed horizontal scroll issue, updated page-size-selector to use primeng,

--- ShiptechV2/libs/feature/quality-control/src/lib/quality-control-grid.module.ts
+++ ShiptechV2/libs/feature/quality-control/src/lib/quality-control-grid.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { AgGridModule } from 'ag-grid-angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AgPagingComponent } from '../../../../core/src/lib/ui/components/ag-grid/ag-paging/ag-paging.component';
+import { AgPagingComponent } from '../../../../core/src/lib/ui/components/ag-paging/ag-paging.component';
 import { CommonModule } from '@angular/common';
 import { PageSizeSelectorComponent } from '../../../../core/src/lib/ui/components/page-size-selector/page-size-selector.component';
+import { PrimeNGModule } from '@shiptech/core';
 
 @NgModule({
   declarations: [AgPagingComponent, PageSizeSelectorComponent],
@@ -13,7 +14,8 @@ import { PageSizeSelectorComponent } from '../../../../core/src/lib/ui/component
     AgGridModule.withComponents([]),
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    PrimeNGModule
   ],
   exports: [AgGridModule, AgPagingComponent, PageSizeSelectorComponent]
 })
