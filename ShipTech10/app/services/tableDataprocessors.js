b6a5a4a7fb107c369a157e6c11663ef5463d4ae8
Alexandru.boasca@24software.ro <Alexandru.boasca@24software.ro>
2019-09-27 13:59:49 +0000

    Change date formatter in schedule dashboard table for deliveryTo and deliveryFrom to format date as UTC
    Workitems:
    #17040 Schedule dashboard table view- Add "Delivery from" and "Delivery to" date columns in grid
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C23491

--- ShipTech10/app/services/tableDataprocessors.js
+++ ShipTech10/app/services/tableDataprocessors.js
@@ -93,10 +93,10 @@ angular.module("shiptech").service("dataProcessors", ['$filtersData', '$state', 
 	            }
 
             }
-            if (obj.name == "claimDate" || obj.name == "orderDate" || /*obj.name == "requestDate" ||*/ obj.name == "date" || obj.name == "quoteDate" || obj.name == "validTo" || obj.name == "validFrom" || (obj.name.indexOf("Date") > -1) || (obj.name.indexOf("date") > -1)) {
+            if (obj.name == "claimDate" || obj.name == "orderDate" || /*obj.name == "requestDate" ||*/ obj.name == "date" || obj.name == "quoteDate" || obj.name == "validTo" || obj.name == "validFrom" || (obj.name.indexOf("Date") > -1) || (obj.name.indexOf("date") > -1) || obj.name == "voyageDetail.deliveryTo" || obj.name == "voyageDetail.deliveryFrom" ) {
                 if (obj.name == "delayInDays" || obj.name.indexOf("delay") > -1) {
                 } else {
-                    if (obj.label == "Date (UTC)" || obj.label == "Due Date" || obj.label == "Working Due Date" || obj.label == "Seller Due Date" || obj.label == "Order Date") {
+                    if (obj.label == "Date (UTC)" || obj.label == "Due Date" || obj.label == "Working Due Date" || obj.label == "Seller Due Date" || obj.label == "Order Date" || obj.name == "voyageDetail.deliveryTo" || obj.name == "voyageDetail.deliveryFrom") {
                         colmodel[key].formatter = CLC.get_formatter("formatDateUtc");
                     } else if(obj.name == 'requestDate') {
 	                    colmodel[key].formatter = CLC.get_formatter("formatDate");
