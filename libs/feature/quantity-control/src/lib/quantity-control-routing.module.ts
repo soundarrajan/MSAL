38a9064f9ff15c2f33ad59c90ba73dde31df709d
Cusnir Dumitru <dumitru.cusnir@24software.ro>
2019-10-21 15:25:38 +0300

    Created knownNamedRouterOutlets, created RelatedLinksFactory, updated QuantityControl Routes

--- libs/feature/quantity-control/src/lib/quantity-control-routing.module.ts
+++ libs/feature/quantity-control/src/lib/quantity-control-routing.module.ts
@@ -2,12 +2,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { MainQuantityControlComponent } from './views/main-quantity-control.component';
 import { PortCallsListComponent } from './views/port-calls-list/port-calls-list.component';
-import {
-  IRelatedLinksRouteData,
-  RelatedLinksComponent
-} from '@shiptech/core/ui/components/related-links/related-links.component';
 import { BlankComponent } from '@shiptech/core/ui/components/blank/blank.component';
 import { PortCallComponent } from './views/port-call/port-call.component';
+import { getRelatedLinksRouteFactory } from '@shiptech/core/ui/components/related-links/related-links.route-factory';
+
+export enum KnownQuantityControlRoutes {
+  portCallsList = 'port-calls',
+  portCallDetails = 'port-call-details',
+  portCallDetailsParams = 'callId',
+}
 
 const routes: Routes = [
   {
@@ -23,43 +26,23 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: 'port-calls',
+        redirectTo: KnownQuantityControlRoutes.portCallsList,
         pathMatch: 'full'
       },
       {
-        path: 'port-calls',
+        path: KnownQuantityControlRoutes.portCallsList,
         component: PortCallsListComponent,
         data: { title: 'Quantity Control', breadcrumb: 'Port Calls' }
       },
       {
-        path: 'port-call-details/:id',
+        path: `${KnownQuantityControlRoutes.portCallDetails}/:${KnownQuantityControlRoutes.portCallDetailsParams}`,
         children: [
           {
             path: '',
             component: PortCallComponent,
-            data: { title: 'Quantity Control - Vessel', breadcrumb: 'Port Calls' }
+            data: { title: 'Quantity Control - Vessel', breadcrumb: 'Port Call' }
           },
-          {
-            outlet: 'breadcrumbs-right',
-            path: '',
-            component: RelatedLinksComponent,
-            data: <IRelatedLinksRouteData>{
-              relatedLinks: {
-                // TODO: Extract to known constant, this should be in Core
-                links: [
-                  { label: 'Request', id: 'request' },
-                  { label: 'Offer', id: 'offer' },
-                  { label: 'Order', id: 'order' },
-                  { label: 'Delivery', id: 'delivery' },
-                  { label: 'Quantity Control', id: 'quantityControl', routerLink: ['./'] },
-                  { label: 'Labs', id: 'labs' },
-                  { label: 'Claims', id: 'claims' },
-                  { label: 'Invoices', id: 'invoices' },
-                  { label: 'Recon', id: 'recon' }
-                ]
-              }
-            }
-          }
+          getRelatedLinksRouteFactory()
         ]
       }
     ]
