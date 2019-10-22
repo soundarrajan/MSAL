96bb3937e2995f0919b0eb55609b90b9f30dd30b
Cusnir Dumitru <dumitru.cusnir@24software.ro>
2019-10-22 16:18:30 +0300

    Added provideIn root for ModuleLoggerFactory

--- ShiptechV2/libs/feature/quantity-control/src/lib/core/logging/module-logger-factory.ts
+++ ShiptechV2/libs/feature/quantity-control/src/lib/core/logging/module-logger-factory.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { ILoggerFactory, LoggerFactory } from '../../../../../../core/src/lib/logging/logger-factory.service';
 import { AppContext } from '@shiptech/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class ModuleLoggerFactory extends LoggerFactory implements ILoggerFactory {
   constructor(appContext: AppContext) {
     super(appContext);
