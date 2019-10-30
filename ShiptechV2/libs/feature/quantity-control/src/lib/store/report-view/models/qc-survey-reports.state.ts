0dd0dd554649efae10057425d4c97a03eb6549f7
Cusnir Dumitru <dumitru.cusnir@24software.ro>
2019-10-30 13:59:37 +0200

    Created AuditLog, EventLog, ProductType, SoundingReport, SurveyReport, VesselReport state models.

---
+++ ShiptechV2/libs/feature/quantity-control/src/lib/store/report-view/models/qc-survey-reports.state.ts
@@ -0,0 +1,8 @@
+import { BaseSubState } from './base.sub-state';
+
+export class QcSurveyReportsState extends BaseSubState {
+  items: QcSurveyReportItemState[];
+}
+
+export class QcSurveyReportItemState {
+}
