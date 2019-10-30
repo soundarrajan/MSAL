cc825ae38cc061d236b8762cd503c5a4b81665b3
Cusnir Dumitru <dumitru.cusnir@24software.ro>
2019-10-30 17:28:15 +0200

    Added AuditLog state

--- libs/feature/quantity-control/src/lib/store/report-view/qc-report.state.model.ts
+++ libs/feature/quantity-control/src/lib/store/report-view/qc-report.state.model.ts
@@ -1,7 +1,9 @@
 import { IQcReportDetailsState } from './details/qc-report-details.state.model';
+import { IQcAuditLogState } from './audit-log/qc-audit-log.state';
 
 export class QcReportStateModel {
   details: IQcReportDetailsState;
+  auditLog: IQcAuditLogState;
 }
 
 export interface IQcReportState extends QcReportStateModel {
