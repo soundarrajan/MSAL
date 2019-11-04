a961fdccc68dbe29bb4513a239441fed59ac3117
Cusnir Dumitru <dumitru.cusnir@24software.ro>
2019-11-04 19:44:51 +0200

    Edit Action update, build error fix

--- libs/feature/quantity-control/src/lib/store/report-view/details/update-product-type.actions.ts
+++ libs/feature/quantity-control/src/lib/store/report-view/details/update-product-type.actions.ts
@@ -1,15 +1,16 @@
-import { QcProductTypeListItemState } from './qc-product-type-list-item.state';
-import { Omit } from '@shiptech/core/utils/type-definitions';
+import { QcProductTypeEditableProps } from '../../../views/qc-report/details/components/port-call-grid/view-model/product-details.view-model';
 
 export class UpdateProductTypeAction {
   static readonly type = '[Qc.Report.Details] - Update product type';
 
-  constructor(public productTypeId: number, public prop: keyof Omit<QcProductTypeListItemState, 'productTypeId' | 'productTypeName'>) {
+  constructor(public productTypeId: number, public prop: QcProductTypeEditableProps, public value: number) {
   }
 
   public log(): any {
     return {
-      productTypeId: this.productTypeId
+      productTypeId: this.productTypeId,
+      prop: this.prop,
+      value: this.value
     };
   }
 }
--- libs/feature/quantity-control/src/lib/views/qc-report/details/components/port-call-grid/view-model/product-details.view-model.ts
+++ libs/feature/quantity-control/src/lib/views/qc-report/details/components/port-call-grid/view-model/product-details.view-model.ts
@@ -6,6 +6,11 @@ import { QcReportState } from '../../../../../../store/report-view/qc-report.sta
 import { ProductTypeListItemViewModel } from './product-type-list-item.view-model';
 import { map } from 'rxjs/operators';
 import { Column } from 'ag-grid-community';
+import { QcReportDetailsService } from '../../../../../../services/qc-report-details.service';
+import { Omit } from '@shiptech/core/utils/type-definitions';
+import { QcProductTypeListItemState } from '../../../../../../store/report-view/details/qc-product-type-list-item.state';
+
+export type QcProductTypeEditableProps = keyof Omit<QcProductTypeListItemState, 'productTypeId' | 'productTypeName'>;
 
 @Injectable()
 export class ProductDetailsViewModel {
@@ -17,7 +22,7 @@ export class ProductDetailsViewModel {
   public numberMaskFormat: string = this.getNumberMaskFormat();
   public numberDisplayFormat: string = `${this.minIntegerDigits}.${this.minFractionDigits}-${this.maxFractionDigits}`;
 
-  constructor(public gridViewModel: ProductDetailsGridViewModel, private store: Store) {
+  constructor(public gridViewModel: ProductDetailsGridViewModel, private store: Store, private detailsService: QcReportDetailsService) {
     this.productTypes$ = this.store.select(QcReportState.getPortCallReportProductTypes).pipe(
       map(productTypes => productTypes.map(productType => new ProductTypeListItemViewModel(productType)))
     );
@@ -32,6 +37,6 @@ export class ProductDetailsViewModel {
   }
 
   public updateProductType(column: Column, model: ProductTypeListItemViewModel, value: number): void {
-    console.log(column.getUserProvidedColDef().field, model.productTypeId, value);
+    this.detailsService.updateProductType(model.productTypeId, <QcProductTypeEditableProps>column.getUserProvidedColDef().field, value);
   }
 }
