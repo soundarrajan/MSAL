f51260171648f6a4c5e7d0359a0b6b6a48e7036f
Narcis Dumitrescu <narcis_dumitrescu@yahoo.com>
2019-11-05 16:43:56 +0200

    Finished Sounding reports master/detail

--- libs/feature/quantity-control/src/lib/views/qc-report/details/components/sounding-reports/view-model/grid-view-model.ts
+++ libs/feature/quantity-control/src/lib/views/qc-report/details/components/sounding-reports/view-model/grid-view-model.ts
@@ -1,59 +1,130 @@
 import { BaseGridViewModel } from '@shiptech/core/ui/components/ag-grid/base.grid-view-model';
 import { AgColumnPreferencesService } from '@shiptech/core/ui/components/ag-grid/ag-column-preferences/ag-column-preferences.service';
 import { ChangeDetectorRef, Injectable } from '@angular/core';
-import { ColDef, GridOptions, IServerSideGetRowsParams } from 'ag-grid-community';
-import { RowModelType } from '@shiptech/core/ui/components/ag-grid/type.definition';
-import { QcSoundingReportListColumns, QcSoundingReportListColumnsLabels } from './grid-columns';
+import { ColDef, GridOptions } from 'ag-grid-community';
+import {
+  QcSoundingReportDetailsColumns,
+  QcSoundingReportDetailsColumnsLabels,
+  QcSoundingReportListColumns,
+  QcSoundingReportListColumnsLabels
+} from './grid-column-constants';
 import { ModuleLoggerFactory } from '../../../../../../core/logging/module-logger-factory';
 import { QcReportDetailsService } from '../../../../../../services/qc-report-details.service';
 import { QcSoundingReportItemModel } from '../../../../../../services/models/qc-sounding-report-item.model';
-import { getShiptechFormatPagination } from '@shiptech/core/grid/server-grid/mappers/shiptech-grid-paging';
-import { getShiptechFormatSorts } from '@shiptech/core/grid/server-grid/mappers/shiptech-grid-sorts';
-import { getShiptechFormatFilters } from '@shiptech/core/grid/server-grid/mappers/shiptech-grid-filters';
-import { QcSoundingReportDetailsGridViewModel } from './details-grid-view-model';
+import { QcSoundingReportDetailsItemModel } from '../../../../../../services/models/qc-sounding-report-details-item.model';
+import { catchError, first, map, switchMap, tap } from 'rxjs/operators';
+import { EMPTY$ } from '@shiptech/core/utils/rxjs-operators';
+import { AppErrorHandler } from '@shiptech/core/error-handling/app-error-handler';
+import { IQcSoundingReportItemDto } from '../../../../../../services/api/dto/qc-report-sounding.dto';
 
 function model(prop: keyof QcSoundingReportItemModel): string {
+  return prop;
+}
+
+function detailsModel(prop: keyof QcSoundingReportDetailsItemModel): string {
   return prop;
 }
 
 @Injectable()
 export class QcSoundingReportListGridViewModel extends BaseGridViewModel {
 
-  detailsGridViewModel =  new QcSoundingReportDetailsGridViewModel(this.quantityControlService);
+  detailsReportIdCol: ColDef = {
+    headerName: QcSoundingReportDetailsColumnsLabels.reportId,
+    colId: QcSoundingReportDetailsColumns.reportId,
+    field: detailsModel('reportId')
+  };
 
-  gridOptions: GridOptions = {
+  tankIdCol: ColDef = {
+    headerName: QcSoundingReportDetailsColumnsLabels.tankId,
+    colId: QcSoundingReportDetailsColumns.tankId,
+    field: detailsModel('tankId')
+  };
+
+  tankNameCol: ColDef = {
+    headerName: QcSoundingReportDetailsColumnsLabels.tankName,
+    colId: QcSoundingReportDetailsColumns.tankName,
+    field: detailsModel('tankName')
+  };
+
+  fuelDescriptionCol: ColDef = {
+    headerName: QcSoundingReportDetailsColumnsLabels.fuelDescription,
+    colId: QcSoundingReportDetailsColumns.fuelDescription,
+    field: detailsModel('fuelDescription')
+  };
+
+  fuelVolumeCol: ColDef = {
+    headerName: QcSoundingReportDetailsColumnsLabels.fuelVolume,
+    colId: QcSoundingReportDetailsColumns.fuelVolume,
+    field: detailsModel('fuelVolume')
+  };
+
+  tankCapacityCol: ColDef = {
+    headerName: QcSoundingReportDetailsColumnsLabels.tankCapacity,
+    colId: QcSoundingReportDetailsColumns.tankCapacity,
+    field: detailsModel('tankCapacity')
+  };
+
+  fuelTempCol: ColDef = {
+    headerName: QcSoundingReportDetailsColumnsLabels.fuelTemp,
+    colId: QcSoundingReportDetailsColumns.fuelTemp,
+    field: detailsModel('fuelTemp')
+  };
+
+  tankUnpumpableVolumeCol: ColDef = {
+    headerName: QcSoundingReportDetailsColumnsLabels.tankUnpumpableVolume,
+    colId: QcSoundingReportDetailsColumns.tankUnpumpableVolume,
+    field: detailsModel('tankUnpumpableVolume')
+  };
+
+  fuelMassCol: ColDef = {
+    headerName: QcSoundingReportDetailsColumnsLabels.fuelMass,
+    colId: QcSoundingReportDetailsColumns.fuelMass,
+    field: detailsModel('fuelMass')
+  };
+
+  detailsGridOptions: GridOptions = {
     groupHeaderHeight: 20,
     headerHeight: 40,
     rowHeight: 35,
-
-    rowModelType: RowModelType.ServerSide,
-    pagination: true,
-
+    pagination: false,
     animateRows: true,
+    multiSortKey: 'ctrl',
+    columnDefs: [
+      this.detailsReportIdCol,
+      this.tankIdCol,
+      this.tankNameCol,
+      this.fuelDescriptionCol,
+      this.fuelVolumeCol,
+      this.tankCapacityCol,
+      this.fuelTempCol,
+      this.tankUnpumpableVolumeCol,
+      this.fuelMassCol
+    ],
+    defaultColDef: {
+      sortable: true,
+      resizable: true
+    }
+  };
 
-    deltaRowDataMode: false,
-    suppressPaginationPanel: false,
-    suppressColumnVirtualisation: true,
-    rowDragManaged: true,
-    suppressRowClickSelection: true,
+  gridOptions: GridOptions = {
+    pagination: false,
+    suppressPaginationPanel: true,
 
+    domLayout: 'autoHeight',
+    animateRows: true,
     multiSortKey: 'ctrl',
 
-    enableBrowserTooltips: true,
-    singleClickEdit: true,
-    getRowNodeId: (data: QcSoundingReportItemModel) => data.reportId.toString(),
-
     masterDetail: true,
+    keepDetailRows: true,
+    keepDetailRowsCount: 100,
     detailCellRendererParams: {
-      detailGridOptions: this.detailsGridViewModel.gridOptions,
-      getDetailRowData: params => this.detailsGridViewModel.detailServerSideGetRows(params)
+      detailGridOptions: this.detailsGridOptions,
+      getDetailRowData: params => this.detailServerSideGetRows(params)
     },
 
     defaultColDef: {
       sortable: true,
-      resizable: true,
-      filter: 'agTextColumnFilter',
-      width: 150
+      resizable: true
     }
   };
 
@@ -158,10 +229,34 @@ export class QcSoundingReportListGridViewModel extends BaseGridViewModel {
     columnPreferences: AgColumnPreferencesService,
     changeDetector: ChangeDetectorRef,
     loggerFactory: ModuleLoggerFactory,
+    private appErrorHandler: AppErrorHandler,
     private quantityControlService: QcReportDetailsService
   ) {
     super('qc-sounding-report-grid', columnPreferences, changeDetector, loggerFactory.createLogger(QcSoundingReportListGridViewModel.name));
     this.initOptions(this.gridOptions);
+
+    // Note: Do note use async pipe to load data directly in template because angular ag-grid first sets rowData to undefined, which shows no-rows and then triggers loading the data.
+    // Note: This would show a glimpse of NoRowsOverlay before actually loading data.
+    this.gridReady$
+      .pipe(
+        first(),
+        tap(() => this.gridApi.showLoadingOverlay()),
+        // Note: No need for pagination or server-side filtering, everything is loaded in memory.
+        switchMap(() => this.quantityControlService.getSoundingReportList({})),
+        catchError(error => {
+          this.gridApi.hideOverlay();
+          return EMPTY$;
+        }),
+        map(response => response.items),
+        tap(items => {
+          if (!items || !items.length) {
+            this.gridApi.showNoRowsOverlay();
+          } else {
+            this.gridApi.setRowData(items);
+            this.gridApi.hideOverlay();
+          }
+        })
+      ).subscribe();
   }
 
   getColumnsDefs(): ColDef[] {
@@ -185,14 +280,12 @@ export class QcSoundingReportListGridViewModel extends BaseGridViewModel {
     ];
   }
 
-  public serverSideGetRows(params: IServerSideGetRowsParams): void {
-    this.quantityControlService.getSoundingReportList({
-      pagination: getShiptechFormatPagination(params),
-      sortList: getShiptechFormatSorts(params),
-      filters: getShiptechFormatFilters(params)
-    }).subscribe(
+  protected detailServerSideGetRows(params: any): void {
+    this.quantityControlService.getSoundingReportListItemDetails((<IQcSoundingReportItemDto>params.data).reportId, {}).subscribe(
       response => params.successCallback(response.items, response.totalItems),
-      () => params.failCallback());
+      error => {
+        this.appErrorHandler.handleError(error);
+      });
   }
 }
 
