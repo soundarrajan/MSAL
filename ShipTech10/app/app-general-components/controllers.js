6fa7de068f377943b11bd53778c68c015e514fe7
Ioana Avram <Ioana.Avram@24software.ro>
2019-11-12 17:59:42 +0000

    Fixed logic for order verification process
    Workitems:
    #15068 Order - Order Verification Process
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C24204

--- ShipTech10/app/app-general-components/controllers.js
+++ ShipTech10/app/app-general-components/controllers.js
@@ -900,22 +900,24 @@ APP_GENERAL_COMPONENTS.controller("Controller_Configurable_List_Control", [
                     return tpl;
                 };
                 var order_list_checkbox = function(cellValue, options, rowObject) {
-                    // var isVerifiedLine = false;
-                    // if (typeof(rowObject.isVerified) != "undefined" && typeof(rowObject.orderStatus) != "undefined") {
-                    //     if (rowObject.isVerified.name == "Yes" || rowObject.orderStatus.displayName == "Cancelled") {
-                    //          isVerifiedLine = true;
-                    //     }
-
-                    // }
-                    // if (procurementSettings.order.orderVerificationReq.id == 1) {
-                    //     if (isVerifiedLine == true) {
-                      //  var tpl = '<label class="mt-checkbox mt-checkbox-outline" style="padding-left: 5px;">' + '<input type="checkbox" class="order_list_checkbox ng-pristine ng-untouched ng-valid ng-empty" rowId="' + options.rowId + '"ng-disabled="true" " ng-model="selectOrders[' + options.rowId + ']"  ng-click="selectOrderListRow(' + options.rowId + ",selectOrders[" + options.rowId + '])"/><span></span></label>';
-                        // } else {
+                    var isVerifiedLine = false;
+                    if (typeof(rowObject.isVerified) != "undefined"  && rowObject.isVerified != null) {
+                        if (rowObject.isVerified.name == "Yes") {
+                             isVerifiedLine = true;
+                        }
+                    }
+                    if (rowObject.orderStatus.displayName == "Cancelled") {
+                        isVerifiedLine = true;
+                    }
+                    if (procurementSettings.order.orderVerificationReq.id == 1) {
+                        if (isVerifiedLine == true) {
+                            var tpl = '<label class="mt-checkbox mt-checkbox-outline" style="padding-left: 5px;">' + '<input type="checkbox" class="order_list_checkbox ng-pristine ng-untouched ng-valid ng-empty" rowId="' + options.rowId + '"ng-disabled="true" " ng-model="selectOrders[' + options.rowId + ']"  ng-click="selectOrderListRow(' + options.rowId + ",selectOrders[" + options.rowId + '])"/><span></span></label>';
+                        } else {
                          var tpl = '<label class="mt-checkbox mt-checkbox-outline" style="padding-left: 5px;">' + '<input type="checkbox" class="order_list_checkbox" rowId="' + options.rowId + '"" ng-model="selectOrders[' + options.rowId + ']"  ng-click="selectOrderListRow(' + options.rowId + ",selectOrders[" + options.rowId + '])"/><span></span></label>';
-
-                        // }
+                        }
                        return tpl;
-                    // /}
+                    }
+        
                 };
                 var contract_planning_email = function(cellValue, options, rowObject) {
                     var tpl = '<a class="font-grey-cascade" ng-click="contractPlanningEmail(' + rowObject.id + ')"><i class="glyphicon glyphicon-envelope"></i></a>';
