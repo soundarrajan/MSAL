486f94d5f612a64f26066aed11e2aa7ca388538e
Ioana Avram <Ioana.Avram@24software.ro>
2019-12-12 08:39:52 +0000

    UI changes for calendar view
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C24698

--- ShipTech10/app/pages/schedule-dashboard-calendar/schedule-dashboard-calendar-component.js
+++ ShipTech10/app/pages/schedule-dashboard-calendar/schedule-dashboard-calendar-component.js
@@ -1188,7 +1188,8 @@ angular.module("shiptech.pages").controller("ScheduleCalendarController", ["$roo
             if (statuses.length > 0) {
                 var statusColor = statusColors.getColorCodeFromLabels(status, $listsCache.ScheduleDashboardLabelConfiguration);
                 return {
-                    "background-color": statusColor
+                    "background-color": statusColor,
+                    "color": getContrastYIQ(statusColor)
                 };
             }
             return null;
@@ -1631,6 +1632,14 @@ angular.module("shiptech.pages").controller("ScheduleCalendarController", ["$roo
                 ctrl.calendar.destroy();
             }
         })
+        function getContrastYIQ(hexcolor){
+            hexcolor = hexcolor.replace("#", "");
+            var r = parseInt(hexcolor.substr(0,2),16);
+            var g = parseInt(hexcolor.substr(2,2),16);
+            var b = parseInt(hexcolor.substr(4,2),16);
+            var yiq = ((r*299)+(g*587)+(b*114))/1000;
+            return (yiq >= 128) ? 'black' : 'white';
+        }
     }
 ]);
 angular.module('shiptech.pages').component('scheduleDashboardCalendar', {
