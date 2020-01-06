42f05dfcb9789adbb84e195dd8bb324ab439af9b
Narcis Dumitrescu <narcis_dumitrescu@yahoo.com>
2020-01-06 23:18:32 +0200

    test git 2 tfs add/remove file

--- libs/core/src/lib/lookups/lookup-sorts.ts
+++
@@ -1,9 +0,0 @@
-export interface ISortsInterface {
-  ColumnValue: string;
-  SortIndex: number;
-  SortParameter: 'asc' | 'desc';
-}
-
-export class SortsDto implements ISortsInterface {
-  constructor(public ColumnValue: string, public SortIndex: number, public SortParameter: 'asc' | 'desc') {}
-}
