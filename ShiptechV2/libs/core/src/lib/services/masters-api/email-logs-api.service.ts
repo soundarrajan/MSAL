1f741daa4d214c5b81e927973910e996a4e6ef60
Radu Coman <radu.coman@24software.ro>
2020-01-14 15:04:15 +0200

    edit email refactor

--- ShiptechV2/libs/core/src/lib/services/masters-api/email-logs-api.service.ts
+++ ShiptechV2/libs/core/src/lib/services/masters-api/email-logs-api.service.ts
@@ -4,28 +4,35 @@ import { HttpClient } from "@angular/common/http";
 import { AppConfig } from "@shiptech/core/config/app-config";
 import { ObservableException } from "@shiptech/core/utils/decorators/observable-exception.decorator";
 import { IEmailLogsRequest, IEmailLogsResponse } from "./request-response-dtos/email-logs.dto";
-import { Observable } from "rxjs";
+import { defer, Observable } from "rxjs";
 import { IEmailLogsApiService } from "./email-logs-api.service.interface";
+import { UrlService } from "@shiptech/core/services/url/url.service";
 
 export namespace EmailLogsApiPaths {
   export const getEmailLogs = () => `api/masters/emaillogs/list`;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class EmailLogsApi implements IEmailLogsApiService {
 
   @ApiCallUrl()
   protected _apiUrl = this.appConfig.v1.API.BASE_URL_DATA_MASTERS;
 
-  constructor(private http: HttpClient, private appConfig: AppConfig) {
+  constructor(private http: HttpClient, private appConfig: AppConfig, private urlService: UrlService) {
   }
 
   @ObservableException()
   getEmailLogs(request: IEmailLogsRequest): Observable<IEmailLogsResponse> {
-    return this.http.post<IEmailLogsResponse>(`${this._apiUrl}/${EmailLogsApiPaths.getEmailLogs()}`, { payload: {...request} });
+    return this.http.post<IEmailLogsResponse>(`${this._apiUrl}/${EmailLogsApiPaths.getEmailLogs()}`, { payload: { ...request } });
+  }
+
+  @ObservableException()
+  editEmail(emailId: number): Observable<unknown> {
+    return defer(() =>
+      window.open(this.urlService.editEmail(emailId)));
   }
 }
 
-export const EMAIL_LOGS_MASTERS_API_SERVICE = new InjectionToken<IEmailLogsApiService>('EMAIL_LOGS_MASTERS_API_SERVICE');
+export const EMAIL_LOGS_MASTERS_API_SERVICE = new InjectionToken<IEmailLogsApiService>("EMAIL_LOGS_MASTERS_API_SERVICE");
