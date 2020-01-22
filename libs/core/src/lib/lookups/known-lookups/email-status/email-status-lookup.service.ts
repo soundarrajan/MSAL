b414a33b9593859500bb27f4cb8fbc3892dedbcc
Radu Coman <radu.coman@24software.ro>
2020-01-22 16:28:53 +0200

    bug fixed

--- libs/core/src/lib/lookups/known-lookups/email-status/email-status-lookup.service.ts
+++ libs/core/src/lib/lookups/known-lookups/email-status/email-status-lookup.service.ts
@@ -1,13 +1,13 @@
-import {LegacyLookupsDatabase} from '@shiptech/core/legacy-cache/legacy-lookups-database.service';
-import {Injectable} from '@angular/core';
-import {AppError} from '@shiptech/core/error-handling/app-error';
-import {nameof} from '@shiptech/core/utils/type-definitions';
-import {EmailStatusLookupEnum, MockEmailStatusLookupEnumMap} from "@shiptech/core/lookups/known-lookups/email-status/email-status-lookup.enum";
-import {IEmailStatusLookupDto} from "@shiptech/core/lookups/known-lookups/email-status/email-status-lookup.interface";
+import { LegacyLookupsDatabase } from "@shiptech/core/legacy-cache/legacy-lookups-database.service";
+import { Injectable } from "@angular/core";
+import { AppError } from "@shiptech/core/error-handling/app-error";
+import { nameof } from "@shiptech/core/utils/type-definitions";
+import { EmailStatusLookupEnum, MockEmailStatusLookupEnumMap } from "@shiptech/core/lookups/known-lookups/email-status/email-status-lookup.enum";
+import { IEmailStatusLookupDto } from "@shiptech/core/lookups/known-lookups/email-status/email-status-lookup.interface";
 
-const nameField = nameof<IEmailStatusLookupDto>('name');
+const nameField = nameof<IEmailStatusLookupDto>("name");
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class EmailStatusLookup {
   get sent(): IEmailStatusLookupDto {
     console.assert(this._sent !== undefined);
@@ -39,25 +39,31 @@ export class EmailStatusLookup {
     this._failed.code = MockEmailStatusLookupEnumMap.Failed.code;
 
     if (!this._failed)
-      throw AppError.MissingLookupKey(nameof<LegacyLookupsDatabase>('emailStatus'), EmailStatusLookupEnum.Failed);
+      throw AppError.MissingLookupKey(nameof<LegacyLookupsDatabase>("emailStatus"), EmailStatusLookupEnum.Failed);
 
     this._sent = await this.legacyLookupsDatabase.emailStatus.where(nameField).equals(EmailStatusLookupEnum.Sent).first();
     // To be removed when color code will be implemented on Backend
     this._sent.code = MockEmailStatusLookupEnumMap.Sent.code;
     if (!this._sent)
-      throw AppError.MissingLookupKey(nameof<LegacyLookupsDatabase>('emailStatus'), EmailStatusLookupEnum.Sent);
+      throw AppError.MissingLookupKey(nameof<LegacyLookupsDatabase>("emailStatus"), EmailStatusLookupEnum.Sent);
 
     this._pending = await this.legacyLookupsDatabase.emailStatus.where(nameField).equals(EmailStatusLookupEnum.Pending).first();
     // To be removed when color code will be implemented on Backend
     this._pending.code = MockEmailStatusLookupEnumMap.Pending.code;
     if (!this._pending)
-      throw AppError.MissingLookupKey(nameof<LegacyLookupsDatabase>('emailStatus'), EmailStatusLookupEnum.Pending);
+      throw AppError.MissingLookupKey(nameof<LegacyLookupsDatabase>("emailStatus"), EmailStatusLookupEnum.Pending);
   }
 
   public toEmailStatus(status: EmailStatusLookupEnum): IEmailStatusLookupDto | undefined {
 
+    const defaultResponse: IEmailStatusLookupDto = {
+      id: null,
+      name: <string>status,
+      displayName:<string>status,
+      code: "#fff"
+    };
     if (status === null || status === undefined)
-      return undefined;
+      return defaultResponse;
 
     switch (status) {
       case EmailStatusLookupEnum.Sent:
@@ -67,10 +73,7 @@ export class EmailStatusLookup {
       case EmailStatusLookupEnum.Pending:
         return this._pending;
       default:
-        return <IEmailStatusLookupDto> {
-          name: <string>status,
-          code: '#fff'
-        };
+        return defaultResponse;
     }
   }
 }
