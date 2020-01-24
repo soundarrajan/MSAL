3653b7407302e8b2d60664eb3a9e2feef078b5ac
Radu Coman <radu.coman@24software.ro>
2020-01-24 17:42:58 +0200

    code review implementation

--- apps/shiptech/src/app/app.module.ts
+++ apps/shiptech/src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { APP_INITIALIZER, NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { bootstrapApplication, BootstrapService } from '@shiptech/core/bootstrap.service';
-import { LoggingModule } from '@shiptech/core/logging/logging.module';
-import { environment } from '@shiptech/environment';
-import { BreadcrumbsModule } from '@shiptech/core/ui/components/breadcrumbs/breadcrumbs.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NgxsModule } from '@ngxs/store';
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-import { DeveloperToolbarModule } from '@shiptech/core/developer-toolbar/developer-toolbar.module';
-import { LoadingBarModule } from '@ngx-loading-bar/core';
-import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
-import { PrimeNGModule } from '@shiptech/core/ui/primeng.module';
-import { AuthenticationModule } from '@shiptech/core/authentication/authentication.module';
-import { CoreModule } from '@shiptech/core/core.module';
-import { APP_BASE_HREF, DOCUMENT } from '@angular/common';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { bootstrapApplication, BootstrapService } from "@shiptech/core/bootstrap.service";
+import { LoggingModule } from "@shiptech/core/logging/logging.module";
+import { environment } from "@shiptech/environment";
+import { BreadcrumbsModule } from "@shiptech/core/ui/components/breadcrumbs/breadcrumbs.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NgxsModule } from "@ngxs/store";
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
+import { DeveloperToolbarModule } from "@shiptech/core/developer-toolbar/developer-toolbar.module";
+import { LoadingBarModule } from "@ngx-loading-bar/core";
+import { LoadingBarRouterModule } from "@ngx-loading-bar/router";
+import { PrimeNGModule } from "@shiptech/core/ui/primeng.module";
+import { AuthenticationModule } from "@shiptech/core/authentication/authentication.module";
+import { CoreModule } from "@shiptech/core/core.module";
+import { APP_BASE_HREF, DOCUMENT } from "@angular/common";
 import { TitleModule } from "@shiptech/core/services/title/title.module";
 
 // Note: Currently we're running the application in a sub directory on the IIS (v2), v1 (angular js) runs in the root. They way we'll also share auth cookies
@@ -52,7 +52,8 @@ export function getAppBaseHref(doc: Document): string {
     NgxsLoggerPluginModule.forRoot({ disabled: environment.production }),
     DeveloperToolbarModule,
     LoadingBarModule,
-    LoadingBarRouterModule
+    LoadingBarRouterModule,
+    TitleModule
   ],
   providers: [
     {
