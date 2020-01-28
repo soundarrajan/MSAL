78e4cbcde93cc6245095bd77bad32f5e2e08b3ae
Radu.coman@24software.ro <Radu.coman@24software.ro>
2020-01-28 13:16:21 +0000

    Git-2-Tfs Sync. f9107fc Merge branch 'master' of https://dev.azure.com/inatech/Shiptech10/_git/Shiptech10-UI; branch: master ***NO_CI***
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C25414

---
+++ v2/shiptech/libs/core/src/lib/services/title/title.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Title } from '@angular/platform-browser';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TitleService {
+  private _title$: BehaviorSubject<string>;
+  public title$: Observable<string>;
+
+  constructor(router: Router, private _browserTitle: Title) {
+    this._title$ = new BehaviorSubject<string>(_browserTitle.getTitle());
+    this.title$ = this._title$.asObservable();
+
+    router.events.pipe(
+      // Note: ActivationEnd event is raised for each route/child route in descending order, e.g childLevel2, childLevel1, root; childLevel2 is leaf root
+      filter(event => event instanceof ActivationEnd && event.snapshot.firstChild === null && event.snapshot.outlet === 'primary'),
+      map((event: ActivationEnd) => event.snapshot.data ?? {}),
+      tap(routeData => this.title = routeData.title)
+    ).subscribe();
+  }
+
+  get title(): string {
+    return this._title$.getValue();
+  }
+
+  set title(value: string) {
+    this._browserTitle.setTitle(value);
+    this._title$.next(value);
+  }
+}
