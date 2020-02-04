5c0adf0668b9d40b0503a9b3dd9b01176f827a5e
Sergiu Vasui <sergiu_vasui@yahoo.com>
2020-02-04 16:14:48 +0200

    Refactor: - selector.component that replaces documents-master-selector.component, vessel-master-selector.component and vessel-port-master-selector.component - Reorganized files and their name for master-autocomplete and master-selector

---
+++ libs/core/src/lib/ui/components/master-selector/masters-models/master-model.interface.ts
@@ -0,0 +1,25 @@
+import {ReplaySubject, Subject} from 'rxjs';
+import {GridOptions} from 'ag-grid-community';
+import {DocumentsMasterSelectorGridViewModel} from '@shiptech/core/ui/components/master-selector/masters-models/documents-model/documents-master-selector-grid.view-model';
+import {VesselMasterSelectorGridViewModel} from '@shiptech/core/ui/components/master-selector/masters-models/vessel-model/vessel-master-selector-grid.view-model';
+import {VesselPortCallsMasterSelectorGridViewModel} from '@shiptech/core/ui/components/master-selector/masters-models/vessel-port-calls-model/vessel-port-calls-master-selector-grid.view-model';
+
+export interface IMasterModelInterface {
+  entityId?: number;
+  entityName?: string;
+  vesselId?: number;
+  selectorType?: string;
+  page: number;
+  isLoading: boolean;
+  pageSize: number;
+  isReady: boolean;
+  isServerSide: boolean;
+  totalPages: number;
+  destroy$: Subject<any>;
+  gridReady$: ReplaySubject<any>;
+  totalItems: number;
+  gridOptions: GridOptions;
+  DocumentsMasterSelectorGridViewModel?: DocumentsMasterSelectorGridViewModel;
+  VesselMasterSelectorGridViewModel?: VesselMasterSelectorGridViewModel;
+  VesselPortCallsMasterSelectorGridViewModel?: VesselPortCallsMasterSelectorGridViewModel;
+}
