d879f5f6866591756db03a352e88b546260f349b
Radu Coman <radu.coman@24software.ro>
2020-02-07 18:20:32 +0200

    remove unused modules and added service workers

--- libs/core/src/lib/services/masters-api/mock-data/vessel-port-calls.mock.ts
+++ libs/core/src/lib/services/masters-api/mock-data/vessel-port-calls.mock.ts
@@ -1,27 +1,27 @@
 import { IVesselPortCallMasterDto } from '@shiptech/core/services/masters-api/request-response-dtos/vessel-port-call';
-import * as _ from 'lodash';
-import * as faker from 'faker';
+import { sample, range } from 'lodash';
+import { name, date, random } from 'faker';
 import { MockLocationsLookup } from '@shiptech/core/services/masters-api/mock-data/locations.mock';
 import { MockServicesLookup } from '@shiptech/core/services/masters-api/mock-data/services.mock';
 
 export function getMockVesselPortCallsEventsLog(n: number): IVesselPortCallMasterDto[] {
-  return _.range(1, n).map(id => getMockVesselPortCallsItem(id, n));
+  return range(1, n).map(id => getMockVesselPortCallsItem(id, n));
 }
 
 export function getMockVesselPortCallsItem(id: number, total: number): IVesselPortCallMasterDto {
-  const name = faker.name.firstName();
+  const firstName = name.firstName();
   return {
     id: id,
-    name: name,
-    displayName: name,
-    eta: faker.date.recent().toISOString(),
-    etb: faker.date.recent().toISOString(),
-    etd: faker.date.recent().toISOString(),
-    location: _.sample(MockLocationsLookup),
-    portCallId: faker.random.number({ min: Math.pow(10, 15), max: Math.pow(10, 16) - 1 }).toString(),
-    service: _.sample(MockServicesLookup),
-    voyageId: faker.random.number({ min: Math.pow(10, 15), max: Math.pow(10, 16) - 1 }),
-    voyageReference: faker.random.alphaNumeric(5).toUpperCase(),
+    name: firstName,
+    displayName: firstName,
+    eta: date.recent().toISOString(),
+    etb: date.recent().toISOString(),
+    etd: date.recent().toISOString(),
+    location: sample(MockLocationsLookup),
+    portCallId: random.number({ min: Math.pow(10, 15), max: Math.pow(10, 16) - 1 }).toString(),
+    service: sample(MockServicesLookup),
+    voyageId: random.number({ min: Math.pow(10, 15), max: Math.pow(10, 16) - 1 }),
+    voyageReference: random.alphaNumeric(5).toUpperCase(),
     totalCount: total,
   };
 }
--- libs/feature/quantity-control/src/lib/services/api/mock/qc-reports-list.mock.ts
+++ libs/feature/quantity-control/src/lib/services/api/mock/qc-reports-list.mock.ts
@@ -1,49 +1,49 @@
 import { IQcReportsListItemDto } from '../dto/qc-reports-list-item.dto';
-import * as faker from 'faker';
-import * as _ from 'lodash';
+import { date, lorem, name, random } from 'faker';
+import { range, sample, values } from 'lodash';
 import { MockStatusLookupEnumMap } from '@shiptech/core/lookups/known-lookups/status/status-lookup.enum';
 import { MockReconStatusLookupEnumMap } from '@shiptech/core/lookups/known-lookups/recon-status/recon-status-lookup.enum';
 
 export function getMockQcReportsList(n: number): IQcReportsListItemDto[] {
-  return _.range(1, n).map(id => getMockQcReportsListItem(id));
+  return range(1, n).map(id => getMockQcReportsListItem(id));
 }
 
 export function getMockQcReportsListItem(id: number): IQcReportsListItemDto {
-  const quantityBefore: number = faker.random.number(5000);
-  const rob: number = faker.random.number(200);
-  const sludge: number = faker.random.number(20);
+  const quantityBefore: number = random.number(5000);
+  const rob: number = random.number(200);
+  const sludge: number = random.number(20);
 
   return {
     id,
-    nbOfMatched: faker.random.number(100),
-    nbOfMatchedWithinLimit: faker.random.number(100),
-    nbOfNotMatched: faker.random.number(100),
-    portCallId: faker.random.number({ min: Math.pow(10, 15), max: Math.pow(10, 16) - 1 }).toString(),
-    portName: faker.name.lastName(),
-    vesselName: faker.random.word(),
-    surveyDate: faker.date.past().toISOString(),
-    surveyStatus: _.sample(_.values(MockStatusLookupEnumMap)),
-    qtyMatchedStatus: _.sample(_.values(MockReconStatusLookupEnumMap)),
+    nbOfMatched: random.number(100),
+    nbOfMatchedWithinLimit: random.number(100),
+    nbOfNotMatched: random.number(100),
+    portCallId: random.number({ min: Math.pow(10, 15), max: Math.pow(10, 16) - 1 }).toString(),
+    portName: name.lastName(),
+    vesselName: random.word(),
+    surveyDate: date.past().toISOString(),
+    surveyStatus: sample(values(MockStatusLookupEnumMap)),
+    qtyMatchedStatus: sample(values(MockReconStatusLookupEnumMap)),
     logBookRobBeforeDelivery: rob * 0.95,
     measuredRobBeforeDelivery: rob * 0.90,
     diffRobBeforeDelivery: rob * 0.7,
     qtyBeforeDeliveryUom: { id: 1, name: 'MT', displayName: 'MT', minTolerance: 2, maxTolerance: 10 },
-    bdnQuantity: faker.random.number(500),
+    bdnQuantity: random.number(500),
     measuredDeliveredQty: quantityBefore - rob - sludge,
-    diffDeliveredQty: faker.random.number(5),
-    qtyDeliveredUom: { id: 1, name: 'MT', displayName: 'MT', minTolerance: 2, maxTolerance: 10  },
+    diffDeliveredQty: random.number(5),
+    qtyDeliveredUom: { id: 1, name: 'MT', displayName: 'MT', minTolerance: 2, maxTolerance: 10 },
     logBookRobAfterDelivery: rob * 1.3,
     measuredRobAfterDelivery: rob * 1.1,
-    diffRobAfterDelivery: faker.random.number(100),
-    qtyAfterDeliveryUom: { id: 1, name: 'MT', displayName: 'MT', minTolerance: 2, maxTolerance: 10  },
+    diffRobAfterDelivery: random.number(100),
+    qtyAfterDeliveryUom: { id: 1, name: 'MT', displayName: 'MT', minTolerance: 2, maxTolerance: 10 },
     logBookSludgeRobBeforeDischarge: rob * 1.1,
     measuredSludgeRobBeforeDischarge: sludge * 0.96,
-    diffSludgeRobBeforeDischarge: faker.random.number(500),
-    sludgeDischargedQty: faker.random.number(500),
-    qtySludgeDischargedUom: { id: 1, name: 'MT', displayName: 'MT', minTolerance: 2, maxTolerance: 10  },
-    comment: faker.lorem.sentence(),
-    isVerifiedSludgeQty: faker.random.boolean(),
-    totalCount: faker.random.number(20),
-    emailTransactionTypeId: faker.random.number(500)
+    diffSludgeRobBeforeDischarge: random.number(500),
+    sludgeDischargedQty: random.number(500),
+    qtySludgeDischargedUom: { id: 1, name: 'MT', displayName: 'MT', minTolerance: 2, maxTolerance: 10 },
+    comment: lorem.sentence(),
+    isVerifiedSludgeQty: random.boolean(),
+    totalCount: random.number(20),
+    emailTransactionTypeId: random.number(500)
   };
 }
