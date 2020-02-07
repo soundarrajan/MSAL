f87ffdf3ee954e549d037e777161ac8a6bc7f189
Radu Coman <radu.coman@24software.ro>
2020-02-07 18:20:32 +0200

    remove unused modules and added service workers

--- ShiptechV2/libs/feature/quantity-control/src/lib/services/api/mock/qc-sounding-report-details.mock.ts
+++ ShiptechV2/libs/feature/quantity-control/src/lib/services/api/mock/qc-sounding-report-details.mock.ts
@@ -1,22 +1,22 @@
-import * as faker from 'faker';
-import * as _ from 'lodash';
+import { commerce, random } from 'faker';
+import { range } from 'lodash';
 import { IQcSoundingReportDetailsItemDto } from '../dto/qc-report-sounding.dto';
 
 export function getMockQcSoundingReportDetails(n: number): IQcSoundingReportDetailsItemDto[] {
-  return _.range(1, n).map(id => getMockQcSoundingReportDetailsItem(id));
+  return range(1, n).map(id => getMockQcSoundingReportDetailsItem(id));
 }
 
 export function getMockQcSoundingReportDetailsItem(id: number): IQcSoundingReportDetailsItemDto {
   return <IQcSoundingReportDetailsItemDto>{
-    fuelDescriptor: faker.commerce.product(),
-    fuelMass: faker.random.number({ min: 5, max: 50 }),
-    fuelTemperature: faker.random.number({ min: 5, max: 100 }),
-    fuelVolume: faker.random.number({ min: 100, max: 2000 }),
-    id: faker.random.number({ min: 1000000, max: 9000000 }),
-    tankCapacity: faker.random.number({ min: 10, max: 200 }),
-    tankId: faker.random.number({ min: 100, max: 2000 }),
-    tankName: faker.commerce.product(),
-    tankUnpumpableVolume: faker.random.number({ min: 0, max: 20 }),
-    measuredVesselReportId: faker.random.number({ min: 1000000, max: 9000000 })
+    fuelDescriptor: commerce.product(),
+    fuelMass: random.number({ min: 5, max: 50 }),
+    fuelTemperature: random.number({ min: 5, max: 100 }),
+    fuelVolume: random.number({ min: 100, max: 2000 }),
+    id: random.number({ min: 1000000, max: 9000000 }),
+    tankCapacity: random.number({ min: 10, max: 200 }),
+    tankId: random.number({ min: 100, max: 2000 }),
+    tankName: commerce.product(),
+    tankUnpumpableVolume: random.number({ min: 0, max: 20 }),
+    measuredVesselReportId: random.number({ min: 1000000, max: 9000000 })
   };
 }
