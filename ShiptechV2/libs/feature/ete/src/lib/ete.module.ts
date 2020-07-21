612261a85c62b362a20c87ba62411644ffdc7f71
Sergiu Vasui <sergiu_vasui@yahoo.com>
2020-07-21 20:45:54 +0300

    Fixed prod build for email template editor Upgraded Angular version from 9.0 to 9.1, typescript and email template editor library version
    Removed unused imports

--- ShiptechV2/libs/feature/ete/src/lib/ete.module.ts
+++ ShiptechV2/libs/feature/ete/src/lib/ete.module.ts
@@ -1,12 +1,12 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {EmailTemplatePageModule} from 'email-template-editor-ckeditor';
-import {MainEteComponent} from "./views/main-ete.component";
-import {EteEditComponent} from "./views/ete-edit/ete-edit.component";
-import {EteRoutingModule} from "./ete-routing.module";
-import {UIModule} from "@shiptech/core/ui/ui.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmailTemplatePageModule } from 'email-template-editor-ckeditor';
+import { MainEteComponent } from './views/main-ete.component';
+import { EteEditComponent } from './views/ete-edit/ete-edit.component';
+import { EteRoutingModule } from './ete-routing.module';
+import { UIModule } from '@shiptech/core/ui/ui.module';
 import { ModuleLoggerFactory } from './core/logging/module-logger-factory';
-import {AuthenticationModule} from "@shiptech/core/authentication/authentication.module";
+import { AuthenticationModule } from '@shiptech/core/authentication/authentication.module';
 
 @NgModule({
   imports: [
@@ -16,14 +16,8 @@ import {AuthenticationModule} from "@shiptech/core/authentication/authentication
     AuthenticationModule.forFeature(),
     EteRoutingModule
   ],
-  declarations: [
-    MainEteComponent,
-    EteEditComponent,
-  ],
+  declarations: [MainEteComponent, EteEditComponent],
   exports: [MainEteComponent],
-  providers: [
-    ModuleLoggerFactory
-  ]
+  providers: [ModuleLoggerFactory]
 })
-export class EteModule {
-}
+export class EteModule {}
