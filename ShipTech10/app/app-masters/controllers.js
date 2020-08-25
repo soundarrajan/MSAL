032dd93601daf8b3bc9f26a4fe26202db91908b5
Ioana Avram <Ioana.Avram@24software.ro>
2020-08-25 15:33:01 +0000

    Fixed warning message after removing one location and add new same location
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C28161

--- ShipTech10/app/app-masters/controllers.js
+++ ShipTech10/app/app-masters/controllers.js
@@ -766,10 +766,10 @@ APP_MASTERS.controller('Controller_Master', [
                     }
                     for (let i = 0; i < $scope.formValues.productsLocations.length; i++) {
                         $scope.formValues.productsLocations[i].systemInstrument = systemInstrument;
-                        if ($scope.formValues.productsLocations[i].isBunkerwireDefault) {
+                        if ($scope.formValues.productsLocations[i].isBunkerwireDefault && !$scope.formValues.productsLocations[i].isDeleted) {
                             let element = $scope.formValues.productsLocations[i];
                             var findCombinationProductAndLocationBunkerwireDefault = _.filter($scope.formValues.productsLocations, function(object) {
-                                return object.location.name == element.location.name && object.product.name == element.product.name && object.isBunkerwireDefault;
+                                return object.isDeleted && object.location.name == element.location.name && object.product.name == element.product.name && object.isBunkerwireDefault;
                             });
                             if (findCombinationProductAndLocationBunkerwireDefault.length > 1) {
                                 products.push(element.product.name);
@@ -778,7 +778,7 @@ APP_MASTERS.controller('Controller_Master', [
                         } else if ($scope.formValues.productsLocations[i].isCargoDefault) {
                             let element = $scope.formValues.productsLocations[i];
                             var findCombinationProductAndLocationCargoDefault = _.filter($scope.formValues.productsLocations, function(object) {
-                                return object.location.name == element.location.name && object.product.name == element.product.name && object.isCargoDefault;
+                                return  object.isDeleted && object.location.name == element.location.name && object.product.name == element.product.name && object.isCargoDefault;
                             });
                             if (findCombinationProductAndLocationCargoDefault.length > 1 ) {
                                 products.push(element.product.name);
