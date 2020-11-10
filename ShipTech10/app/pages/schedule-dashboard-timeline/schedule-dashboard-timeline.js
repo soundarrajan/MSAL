04bf1b8de690b6b11ec1fef0e08215518c4c7690
Ioana Avram <Ioana.Avram@24software.ro>
2020-11-10 07:22:29 +0000

    Fixed popup in timeline screen
    Workitems:
    #23871 UI - Timeline view - Responsiveness of moving across voyages is slow
    #28419 Timeline view - Right click Pop up closes when we move mouse pointer outside pop up
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C28598

--- ShipTech10/app/pages/schedule-dashboard-timeline/schedule-dashboard-timeline.js
+++ ShipTech10/app/pages/schedule-dashboard-timeline/schedule-dashboard-timeline.js
@@ -1835,17 +1835,11 @@ angular.module("shiptech.pages").controller("ScheduleTimelineController", ["$sco
             console.log(ctrl);
         });
 
-        $(document).on("mouseover", "div.vis-group", function(event){
-            if ((!$(event.target).hasClass("contextmenu") && !$(event.target).parents('.contextmenu').length) || $(event.target).hasClass("close")) {
-                $(".contextmenu").css("display", "none");
-            }
-        });
 
         /*build hover popover*/
         $(document).on("mouseover", "span[voyage-detail-id]", function(){
 
             console.log("mouseover");
-            $(".contextmenu").css("display", "none");
             var voyageDetailId = $(this).attr("voyage-detail-id");
             if (voyageDetailId) {
                 var html = buildHoverPopoverMarkup(voyageDetailId);
@@ -1858,6 +1852,8 @@ angular.module("shiptech.pages").controller("ScheduleTimelineController", ["$sco
                     template: '<div class="popover" style="z-index: 9999999" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body timeline-popover-hover">'+html+'</div></div>'
                 }).
                 on('show.bs.popover', function (event) {
+                    $scope.rightClickPopoverData = null;
+                    $scope.$apply();
                     var lengthVoyageStops = getLengthPopoverMarkup(voyageDetailId);
                     if (lengthVoyageStops > 3) {
                         $('.breadcrumbs-container').css('z-index', '0');
