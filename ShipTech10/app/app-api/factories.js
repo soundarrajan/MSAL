acaef58c1972e9121af06c8d2cb8257a7ed5af0b
Prathiban T <prathiban.t@inatech.com>
2021-03-08 12:09:27 +0000

    Vessel product tank model - name prop added; factory fetch for customer type counterparties
    Workitems:
    #26032 Vessel Master - New field to be added
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C29386

--- ShipTech10/app/app-api/factories.js
+++ ShipTech10/app/app-api/factories.js
@@ -7702,7 +7702,44 @@ APP_API.factory('$Api_Service', [
                             }
                         );
                         return;
-                    }                    
+                    }
+                    if (param.field.clc_id == 'masters_counterpartylist_customers') {
+                        var apiJSON = {
+                            Payload: {
+                                Filters: [
+                                    {
+                                        ColumnName: 'CounterpartyTypes',
+                                        Value: 4
+                                    }
+                                ]
+                            }
+                        };
+                        var url = `${API.BASE_URL_DATA_MASTERS }/api/masters/counterparties/listByTypesAutocomplete`;
+                        $http.post(url, apiJSON).then(
+                            (response) => {
+                                if (response.status == 200) {
+                                    callback(response.data.payload);
+                                } else {
+                                    callback([
+                                        {
+                                            id: -1,
+                                            name: 'HTTP Error!'
+                                        }
+                                    ]);
+                                }
+                            },
+                            (response) => {
+                                console.log('$APIService dropdown.lookup failed for parameter ', param.field.masterSource);
+                                callback([
+                                    {
+                                        id: -1,
+                                        name: 'HTTP Error!'
+                                    }
+                                ]);
+                            }
+                        );
+                        return;
+                    }
                     if (param.app == 'delivery' && param.screen == 'delivery' && param.field.masterSource == 'OrderList') {
                         var apiJSON = {
                             UIFilters: {
