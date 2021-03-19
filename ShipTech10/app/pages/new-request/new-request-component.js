2ca86db1c1059a3b3148c3918e334df028ddb6ef
Kishore Kumar Lakshmanan <kishore.k@inatech.com>
2021-03-19 11:07:50 +0000

    Requirement 29837: Request - 2M Delivery & Vessel ISO in the Request Screen Removed Controller function-Issue Fixed
    Workitems:
    #29837 Request - 2M Delivery & Vessel ISO in the Request Screen
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C29571

--- ShipTech10/app/pages/new-request/new-request-component.js
+++ ShipTech10/app/pages/new-request/new-request-component.js
@@ -924,13 +924,7 @@ angular.module('shiptech.pages').controller('NewRequestController', [
                 }
             });
         };
-        $scope.vesselIsoSpecGroupName=function(){
-           var vesselGroupName= ctrl.request.vesselDetails.vessel.vesselIsoSpecGroup.name ;
-           var splitString=vesselGroupName.split(" ",1);
-           return splitString.toString();
-           
-        }
-
+       
 
         ctrl.showDeleteLocationConfirm = function(location) {
             ctrl.entityToDelete = 'location';
