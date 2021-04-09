78afcfaefbb516494b6eae97ffd68f6fa8feda55
Avram Ioana <Ioana.Avram@24software.ro>
2021-04-09 09:56:22 +0300

    Added supplier lookup and deal date

--- ShiptechV2/libs/feature/contract/src/lib/views/contract/details/static-lists-route.resolver.ts
+++ ShiptechV2/libs/feature/contract/src/lib/views/contract/details/static-lists-route.resolver.ts
@@ -41,7 +41,7 @@ export class StaticListsRouteResolver implements Resolve<any> {
       ]);
     }
     return  this.contractService.getStaticLists(['Company','Seller', 'PaymentTerm', 
-      'Incoterm', 'ApplyTo', 'ContractualQuantityOption', 'Uom']);
+      'Incoterm', 'ApplyTo', 'ContractualQuantityOption', 'Uom', 'UomMass', 'UomVolume', 'ContractConversionFactorOptions']);
 
   }
 
