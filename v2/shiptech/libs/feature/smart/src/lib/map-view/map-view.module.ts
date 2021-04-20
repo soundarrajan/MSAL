48429407929fe9a26d27f98381f8886adf9e68db
00000002-0000-8888-8000-000000000000@2c895908-04e0-4952-89fd-54b0046d6288 <00000002-0000-8888-8000-000000000000@2c895908-04e0-4952-89fd-54b0046d6288>
2021-04-20 02:01:33 +0000

    Git-2-Tfs Sync. 9ac1a2f Merged PR 9960: Requirement 29806, Requirement 29778 related changes has done; branch: master ***NO_CI***
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C29987

--- v2/shiptech/libs/feature/smart/src/lib/map-view/map-view.module.ts
+++ v2/shiptech/libs/feature/smart/src/lib/map-view/map-view.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthenticationModule } from '@shiptech/core/authentication/authentication.module';
 import { MaterialModule } from '../material-module'; 
 import { SharedModule } from '../shared/shared.module';
 import { MapViewRoutingModule } from './map-view-routing.module';
@@ -13,6 +14,7 @@ import { PortDetailsComponent } from './port-details/port-details.component';
   declarations: [MapViewHomeComponent, VesselDetailsComponent, ComponentsComponent, PortDetailsComponent],
   imports: [
     CommonModule,
+    AuthenticationModule.forFeature(),
     MapViewRoutingModule,
     MaterialModule,
     SharedModule
