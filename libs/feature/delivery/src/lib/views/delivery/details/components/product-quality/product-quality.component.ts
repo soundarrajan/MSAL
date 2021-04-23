f47c4fac60ceeece10970e3805d02689d1394deb
Avram Ioana <Ioana.Avram@24software.ro>
2021-04-23 10:34:48 +0300

    Allowed negative values in delivery screen fields

--- libs/feature/delivery/src/lib/views/delivery/details/components/product-quality/product-quality.component.ts
+++ libs/feature/delivery/src/lib/views/delivery/details/components/product-quality/product-quality.component.ts
@@ -336,7 +336,7 @@ export class ProductQualityComponent extends DeliveryAutocompleteComponent
   // Only Number
   keyPressNumber(event) {
     var inp = String.fromCharCode(event.keyCode);
-    if (inp == '.' || inp == ',') {
+    if (inp == '.' || inp == ',' || inp == '-') {
       return true;
     }
     if (/^[-,+]*\d{1,6}(,\d{3})*(\.\d*)?$/.test(inp)) {
