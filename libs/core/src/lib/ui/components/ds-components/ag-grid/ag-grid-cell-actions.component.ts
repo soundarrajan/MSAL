7a71bd9c279e11a8b2665387df0fdd51f82ac822
Gokul Dhas Simson <gokul.s@Inatech.com>
2021-04-26 11:27:51 +0530

    Build Issue Fix

--- libs/core/src/lib/ui/components/ds-components/ag-grid/ag-grid-cell-actions.component.ts
+++ libs/core/src/lib/ui/components/ds-components/ag-grid/ag-grid-cell-actions.component.ts
@@ -113,7 +113,7 @@ export class AGGridCellActionsComponent implements ICellRendererAngularComp {
         });
     }
 
-    navigateTo() {
+    navigateTo(event) {
         this.params.onClick(this.params);
     }
 
