d34ab6db477214b1e18b4be4837cb26c27f8a203
udayakumar.s <udayakumar.s@inatech.com>
2021-04-28 19:00:03 +0530

    #custom from to date picker header part for bunkering plan page has done

---
+++ libs/feature/smart/src/lib/shared/date-picker-from-to/date-picker-from-to.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import { DateAdapter } from '@angular/material/core';
+
+@Component({
+  selector: 'shiptech-date-picker-from-to',
+  templateUrl: './date-picker-from-to.component.html',
+  styleUrls: ['./date-picker-from-to.component.css']
+})
+export class DatePickerFromToComponent implements OnInit {
+  
+  @Input() selectedFromDate: Date;
+  @Input() selectedToDate: Date;
+  @Output() onDateChange = new EventEmitter<any>();
+  fromDate = new FormControl();
+  toDate = new FormControl();
+
+  constructor(private dateAdapter: DateAdapter<Date>) { 
+    this.dateAdapter.setLocale('en-GB');
+  }
+
+  ngOnInit(): void {
+    this.selectedFromDate = this.selectedFromDate? new Date(this.selectedFromDate): new Date();
+    this.selectedToDate = this.selectedToDate? new Date(this.selectedToDate): new Date();
+    this.fromDate = new FormControl(this.selectedFromDate);
+    this.toDate = new FormControl(this.selectedToDate);
+  }
+
+  onDatePickerChange(inputMode, event) {
+    
+  let selectedDate = {
+    fromDate: this.fromDate?.value,
+    toDate: this.toDate?.value
+  }
+  this.onDateChange.emit(selectedDate);
+  }
+
+
+}
