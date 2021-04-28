b4736169c40d3be242ae935d05863616b095c68c
Ioana Avram <Ioana.Avram@24software.ro>
2021-04-28 12:54:53 +0000

    Git-2-Tfs Sync. c87d9e5 Merged PR 10242: Disabled formula fields in the Contract when has Invoice approved; branch: master ***NO_CI***
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C30133

--- v2/shiptech/libs/feature/contract/src/lib/views/contract/details/components/event-based-simple/event-based-simple.component.ts
+++ v2/shiptech/libs/feature/contract/src/lib/views/contract/details/components/event-based-simple/event-based-simple.component.ts
@@ -95,6 +95,7 @@ export class EventBasedSimple extends DeliveryAutocompleteComponent
   isValidFromDateInvalid: boolean;
   businessCalendarList: any;
   formulaEventIncludeList: any;
+  hasInvoicedOrder: any;
  
 
 
@@ -228,6 +229,14 @@ export class EventBasedSimple extends DeliveryAutocompleteComponent
     } 
     this.formulaEventIncludeList = formulaEventIncludeList;
   }
+
+  @Input('hasInvoicedOrder') set _setHasInvoicedOrder(hasInvoicedOrder) { 
+    if (!hasInvoicedOrder) {
+      return;
+    } 
+    this.hasInvoicedOrder = hasInvoicedOrder;
+  }
+
 
 
   @Input() events: Observable<void>;
