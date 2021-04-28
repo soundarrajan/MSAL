437a1e964ba6ccc36ae5c4a780202c1a1eb9ab99
Avram Ioana <Ioana.Avram@24software.ro>
2021-04-28 17:37:31 +0300

    Fixed payload of getProdDefaultConversionFactors

--- libs/feature/contract/src/lib/views/contract/details/components/contract-product/contract-product.component.ts
+++ libs/feature/contract/src/lib/views/contract/details/components/contract-product/contract-product.component.ts
@@ -1333,7 +1333,7 @@ export class ContractProduct extends DeliveryAutocompleteComponent
       }
     }
     if (!isAlreadyAdded && indexDeleted == -1 && selectedProduct) {
-      payload = { Payload: selectedProduct.product.id };
+      payload = {Payload: { ProductId: selectedProduct.product.id} };
       this.spinner.show();
       this.contractService
       .getProdDefaultConversionFactors(payload)
