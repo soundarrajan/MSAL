75d37a7ac097a9d21a9c1d75b260e61deebaba45
Avram Ioana <Ioana.Avram@24software.ro>
2021-05-12 09:26:45 +0300

    Changed product-details component

--- libs/feature/invoice/src/lib/invoice-routing.module.ts
+++ libs/feature/invoice/src/lib/invoice-routing.module.ts
@@ -7,6 +7,7 @@ import { MainInvoiceComponent } from './views/main-invoice.component';
 import { InvoiceListComponent } from './views/invoice-list/invoice-list.component';
 import { KnownInvoiceRoutes } from './known-invoice.routes';
 import { InvoiceViewComponent } from './views/invoice-view/invoice-view.component';
+import { StaticListsRouteResolver } from './views/invoice-view/details/static-lists-route.resolver';
 
 const routes: Routes = [
   {
@@ -36,7 +37,8 @@ const routes: Routes = [
             path: `:${KnownInvoiceRoutes.InvoiceIdParam}`,
             component: InvoiceViewComponent,
             resolve:{
-              navBar: NavBarResolver
+              navBar: NavBarResolver,
+              staticLists: StaticListsRouteResolver
             },
             data: { title: 'View Invoice', breadcrumb: 'View Invoice' }
           },
