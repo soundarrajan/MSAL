fff9ae51cd8d8485491c64e172a1a4db017a8102
Rajapandian Soundrapandian <rajapandian.s@inatech.com>
2021-05-12 08:13:38 +0000

    Merged PR 10633: Additonal cost view only
    Additonal cost view only

--- libs/feature/invoice/src/lib/views/invoice-view/details/component/additional-cost-modal/additional-cost-modal.component.ts
+++ libs/feature/invoice/src/lib/views/invoice-view/details/component/additional-cost-modal/additional-cost-modal.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'shiptech-additional-cost-modal',
   templateUrl: './additional-cost-modal.component.html',
   styleUrls: ['./additional-cost-modal.component.scss']
 })
-export class AdditionalCostModalComponent implements OnInit {
 
+export class AdditionalCostModalComponent implements OnInit {
+  additionalCost : any;
+  @Input('formValues') set _formValues(val){
+    this.additionalCost = val
+  }
   public searchText:string;
   selectedRow;
   public costData:any = [{selected:false, costType:'Barging'}, 
                             {selected:false, costType:'Tax'},
                             {selected:false, costType:'Miscellaneous Charges'}];
-  constructor(public dialogRef: MatDialogRef<AdditionalCostModalComponent>) { 
+  constructor() { 
     
   }
 
