d72d8ffd90251c005806dd757ad0b1d2d66e249c
Rajapandian Soundrapandian <rajapandian.s@inatech.com>
2021-05-12 15:25:29 +0000

    Merged PR 10669: Additional cost changes
    Additional cost changes

--- ShiptechV2/libs/feature/invoice/src/lib/views/invoice-view/details/component/additional-cost-modal/additional-cost-modal.component.ts
+++ ShiptechV2/libs/feature/invoice/src/lib/views/invoice-view/details/component/additional-cost-modal/additional-cost-modal.component.ts
@@ -9,15 +9,63 @@ import { LegacyLookupsDatabase } from '@shiptech/core/legacy-cache/legacy-lookup
 
 export class AdditionalCostModalComponent implements OnInit {
   additionalCost : any;
+  productDetails : any;
   @Output() changedAdditonalcost = new EventEmitter();
   @Input('formValues') set _formValues(val){
-    this.additionalCost = val
+    this.additionalCost = val.costDetails;
+    this.productDetails = [{id:'All',name:"All"}];
+    val.productDetails.forEach(element => {
+      if(element.product){
+        // element.product.forEach(product => {
+          this.productDetails.push({id:element.product.id+'',name:element.product.name})
+        // });
+      }
+    });
   }
   costNames:any;
   uomNames:any;
+  currencyNames:any;
   public searchText:string;
   selectedRow;
   public costType:any = [{id:1, name: "Flat"},{id:2, name: "Unit"}];
+  newCostItem={
+    amountInInvoiceCurrency: 0,
+    amountInOrderCurrency: 0,
+    associatedOrderProduct: "",
+    clientIpAddress: null,
+    costName: {id: 0, name: "", internalName: null, displayName: null, code: null, collectionName: null},
+    costType: {id: 0, name: "", internalName: null, displayName: null, code: null, collectionName: null},
+    deliveryProductId: 0,
+    deliveryQuantity: 0,
+    deliveryQuantityUom: {id: 0, name: "", internalName: null, displayName: null, code: null, collectionName: null},
+    description: null,
+    difference: 0,
+    estimatedAmount: 0,
+    estimatedExtras: 0,
+    estimatedExtrasAmount: 0,
+    estimatedRate: 0,
+    estimatedRateCurrency: {id: 0, name: "US dollar", internalName: null, displayName: null, code: null, collectionName: null},
+    estimatedRateUom: null,
+    estimatedTotalAmount: 0,
+    id: 0,
+    invoiceAmount: 0,
+    invoiceExtras: 0,
+    invoiceExtrasAmount: 0,
+    invoiceQuantity: 0,
+    invoiceQuantityUom: {id: 0, name: "", internalName: null, displayName: null, code: null, collectionName: null},
+    invoiceRate: 0,
+    invoiceRateCurrency: {id: 0, name: "", internalName: null, displayName: null, code: "", collectionName: null},
+    invoiceRateUom: {id: 0, name: "", internalName: null, displayName: null, code: null, collectionName: null},
+    invoiceTotalAmount: 0,
+    isAllProductsCost: false,
+    isDeleted: false,
+    isOrderCost: true,
+    modulePathUrl: null,
+    orderAdditionalCostId: null,
+    product: {id: 0, name: "", internalName: null, displayName: "", code: null},
+    reconStatus: {transactionTypeId: null, id: 0, name: "", internalName: null, displayName: null, code: null},
+    userAction: null
+  }
   constructor(private legacyLookupsDatabase: LegacyLookupsDatabase,private changeDetectorRef: ChangeDetectorRef) { 
     
   }
@@ -31,10 +79,14 @@ export class AdditionalCostModalComponent implements OnInit {
       this.uomNames = list;
       this.changeDetectorRef.detectChanges();
     })
+    this.legacyLookupsDatabase.getCurrencyTable().then(list=>{
+      this.currencyNames = list;
+      this.changeDetectorRef.detectChanges();
+    })
   }
   addNewAdditionalCostLine(){
-    console.log("add additional cost")
-    // this.additionalCost.push({id:0,isDeleted:false});
+    this.additionalCost.push(this.newCostItem);
+    this.changedAdditonalCostEmit()
   }
   removeAdditionalCostLine(index){
     this.additionalCost[index].isDeleted = true;
@@ -53,6 +105,18 @@ export class AdditionalCostModalComponent implements OnInit {
   }
 
   applyFilter() {
+  }
+  invoiceRateChange(event,index){
+    let sumvalue = +event;
+    this.additionalCost[index].amountInOrderCurrency =  this.additionalCost[index].costType.id == 2 ? sumvalue * this.additionalCost[index].invoiceQuantity : sumvalue;     
+    this.additionalCost[index].invoiceExtrasAmount = this.additionalCost[index].costType.id == 2 ? this.additionalCost[index].amountInOrderCurrency * this.additionalCost[index].invoiceExtras/100 : this.additionalCost[index].amountInOrderCurrency * this.additionalCost[index].invoiceExtras/100;
+    this.changedAdditonalCostEmit();
+  }
+  extraAmount(event,index){    
+    let sumValue = +event;
+    this.additionalCost[index].invoiceExtrasAmount = this.additionalCost[index].costType.id == 2 ? this.additionalCost[index].amountInOrderCurrency * this.additionalCost[index].invoiceExtras/100 : this.additionalCost[index].amountInOrderCurrency * this.additionalCost[index].invoiceExtras/100;
+    // alert(index);
+    this.changedAdditonalCostEmit();
   }
   changedAdditonalCostEmit(){
     this.changedAdditonalcost.emit(this.additionalCost);
