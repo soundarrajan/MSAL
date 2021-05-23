82eedb84b0bec4ccbe873f228dacd0fb177c29f1
Divya Premachandran <divya.p@inatech.com>
2021-05-23 22:25:12 +0000

    Git-2-Tfs Sync. 9223bda Merged PR 10858: SOA calculation for Current Bunkering plan; branch: master ***NO_CI***
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C30567

--- v2/shiptech/libs/feature/smart/src/lib/store/bunker-plan/bunkering-plan.state.ts
+++ v2/shiptech/libs/feature/smart/src/lib/store/bunker-plan/bunkering-plan.state.ts
@@ -29,7 +29,7 @@ export class AddCurrentBunkeringPlanStateModel{
     BPlanData: []
   }
 })
-
+//This state is meant to store values required to SAVE Current bunkering plan
 @Injectable()
 export class SaveBunkeringPlanState{
 
@@ -213,19 +213,15 @@ export class SaveBunkeringPlanState{
     CurrentBplanData: []
   }
 })
-
+//This state is meant to store values required for SOA Calculation in Current Bunkering Plan
 @Injectable()
 export class AddCurrentBunkeringPlanState{
 
-  // @Selector([AddCurrentBunkeringPlanState])
-  // static getCurrentBunkeringPlanData(state: AddCurrentBunkeringPlanStateModel):AddCurrentBunkeringPlanModel[]{
-  //   return state?.CurrentBplanData
-  // }
+  @Selector([AddCurrentBunkeringPlanState])
+  static getCurrentBunkeringPlanData(state: AddCurrentBunkeringPlanStateModel):AddCurrentBunkeringPlanModel[]{
+    return state?.CurrentBplanData
+  }
 
-  // @Selector([AddCurrentBunkeringPlanState])
-  // static getFirstCurrentBunkeringPlanData(state: AddCurrentBunkeringPlanStateModel):AddCurrentBunkeringPlanModel{
-  //   return state?.CurrentBplanData[0];
-  // }
 
   @Selector([AddCurrentBunkeringPlanState])
   static getCBPhsfo05_stock(state: AddCurrentBunkeringPlanStateModel):any{
@@ -241,84 +237,56 @@ export class AddCurrentBunkeringPlanState{
     })
   }
 
-  // @Action(UpdateCurrentBunkeringPlanAction)
-  // update({getState, patchState}: StateContext<AddCurrentBunkeringPlanStateModel>, {payload, type, detail_no}:UpdateCurrentBunkeringPlanAction){
-  //   const state = getState();
-  //   let BPlanData = JSON.parse(JSON.stringify(state.CurrentBplanData));
-  //   let BPlanDataIndex = BPlanData.findIndex(data => data.detail_no == detail_no)
-  //   switch(type){
-  //     case 'hsfo_max_lift':{
-  //                             BPlanData[BPlanDataIndex].hsfo_max_lift = payload;
-  //                             break;
-  //                           }
-  //     case 'hsfo_estimated_consumption':{
-  //                                         BPlanData[BPlanDataIndex].hsfo_estimated_consumption = payload;
-  //                                         break;
-  //                                       }
-  //     case 'hsfo_safe_port':{
-  //                             BPlanData[BPlanDataIndex].hsfo_safe_port = payload;
-  //                             break;
-  //                           }
-  //     case 'hsfo05_stock':{
-  //                           BPlanData[BPlanDataIndex].hsfo05_stock = payload;
-  //                           break;
-  //                         }
-  //     case 'hsfo_soa':{
-  //                           BPlanData[BPlanDataIndex].hsfo_soa = payload;
-  //                           break;
-  //                         }                    
-  //     case 'eca_estimated_consumption':{
-  //                                         BPlanData[BPlanDataIndex].eca_estimated_consumption = payload;
-  //                                         break;
-  //                                       }
-  //     case 'eca_safe_port':{
-  //                             BPlanData[BPlanDataIndex].eca_safe_port = payload;
-  //                             break;
-  //                           }
-  //     case 'ulsfo_max_lift':{
-  //                             BPlanData[BPlanDataIndex].ulsfo_max_lift = payload;
-  //                             break;
-  //                           }
-  //     case 'ulsfo_estimated_lift':{
-  //                             BPlanData[BPlanDataIndex].ulsfo_estimated_lift = payload;
-  //                             break;
-  //                           }
-  //     case 'ulsfo_soa':{
-  //                             BPlanData[BPlanDataIndex].ulsfo_soa = payload;
-  //                             break;
-  //                           }
-  //     case 'lsdis_max_lift':{
-  //                             BPlanData[BPlanDataIndex].lsdis_max_lift = payload;
-  //                             break;
-  //                           }
-  //     case 'lsdis_estimated_consumption':{
-  //                                         BPlanData[BPlanDataIndex].lsdis_estimated_consumption = payload;
-  //                                         break;
-  //                                       }
-  //     case 'lsdis_safe_port':{
-  //                               BPlanData[BPlanDataIndex].lsdis_safe_port = payload;
-  //                               break;
-  //                             }
-  //     case 'lsdis_estimated_lift':{
-  //                               BPlanData[BPlanDataIndex].lsdis_estimated_lift = payload;
-  //                               break;
-  //                             }
-  //     case 'lsdis_as_eca':{
-  //                               BPlanData[BPlanDataIndex].lsdis_as_eca = payload;
-  //                               break;
-  //                             }
-  //     case 'lsdis_soa':{
-  //                               BPlanData[BPlanDataIndex].lsdis_soa = payload;
-  //                               break;
-  //                             }
-  //   }
+  @Action(UpdateCurrentBunkeringPlanAction)
+  update({getState, patchState}: StateContext<AddCurrentBunkeringPlanStateModel>, {payload, type, detail_no}:UpdateCurrentBunkeringPlanAction){
+    const state = getState();
+    let BPlanData = JSON.parse(JSON.stringify(state.CurrentBplanData));
+    let BPlanDataIndex = BPlanData.findIndex(data => data.detail_no == detail_no)
+    switch(type){
+      case 'hsfo_estimated_consumption':{
+                                          BPlanData[BPlanDataIndex].hsfo_estimated_consumption = payload;
+                                          break;
+                                        }
+      case 'hsfo05_stock':{
+                            BPlanData[BPlanDataIndex].hsfo05_stock = payload;
+                            break;
+                          }
+      case 'hsfo_soa':{
+                            BPlanData[BPlanDataIndex].hsfo_soa = payload;
+                            break;
+                          }                    
+      case 'eca_estimated_consumption':{
+                                          BPlanData[BPlanDataIndex].eca_estimated_consumption = payload;
+                                          break;
+                                        }
+      case 'ulsfo_estimated_lift':{
+                              BPlanData[BPlanDataIndex].ulsfo_estimated_lift = payload;
+                              break;
+                            }
+      case 'ulsfo_soa':{
+                              BPlanData[BPlanDataIndex].ulsfo_soa = payload;
+                              break;
+                            }
+      case 'lsdis_estimated_consumption':{
+                                          BPlanData[BPlanDataIndex].lsdis_estimated_consumption = payload;
+                                          break;
+                                        }
+      case 'lsdis_as_eca':{
+                                BPlanData[BPlanDataIndex].lsdis_as_eca = payload;
+                                break;
+                              }
+      case 'lsdis_soa':{
+                                BPlanData[BPlanDataIndex].lsdis_soa = payload;
+                                break;
+                              }
+    }
     
-  //   patchState({
-  //     ...state,
-  //     CurrentBplanData: BPlanData
+    patchState({
+      ...state,
+      CurrentBplanData: BPlanData
       
-  //   })
-  // }
+    })
+  }
 }
 
 export class SaveCurrentROBStateModel{
