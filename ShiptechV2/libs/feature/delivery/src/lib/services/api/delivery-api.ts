6dd424c36bc20625bffc4997a448c4d3b69aeada
Suresh R <suresh.r@Inatech.com>
2021-05-24 14:23:28 +0530

    Delivery Notes

--- ShiptechV2/libs/feature/delivery/src/lib/services/api/delivery-api.ts
+++ ShiptechV2/libs/feature/delivery/src/lib/services/api/delivery-api.ts
@@ -110,7 +110,8 @@ export class DeliveryApi implements IDeliveryApiService {
       `${this._apiUrl}/${DeliveryApiPaths.getDeliveryDetails()}`,
       { payload: request }
     ).pipe(
-      map((body: any) => body.payload),
+      map((body: any) => body.payload
+      ),
       catchError((body: any) => of(body.error.ErrorMessage && body.error.Reference ? body.error.ErrorMessage + ' ' + body.error.Reference : body.error.errorMessage + ' ' + body.error.reference))
     );
   }
@@ -203,6 +204,7 @@ export class DeliveryApi implements IDeliveryApiService {
       catchError((body: any) => of(body.error.ErrorMessage && body.error.Reference ? body.error.ErrorMessage + ' ' + body.error.Reference : body.error.errorMessage + ' ' + body.error.reference))
     );
   }
+  
 
   @ObservableException()
   saveNotesDelivery(
@@ -211,8 +213,12 @@ export class DeliveryApi implements IDeliveryApiService {
     return this.http.post<IDeliveryNotesDetailsResponse>(
       `${this._apiUrl}/${DeliveryApiPaths.saveNotesDelivery()}`,
       { payload: request }
-    ).pipe(
-      map((body: any) => body.upsertedId),
+    )
+    .pipe(
+      map((body: any) => body.payload
+       
+      ),
+      
       catchError((body: any) => of(body.error.ErrorMessage && body.error.Reference ? body.error.ErrorMessage + ' ' + body.error.Reference : body.error.errorMessage + ' ' + body.error.reference))
     );
   }
