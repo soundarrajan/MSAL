a36d0831874479f8bcbbbdef753cb3ca43de750f
udaya kumar s <udayakumar.s@inatech.com>
2021-05-25 13:26:26 +0000

    Git-2-Tfs Sync. 43a5940 Merge branch 'master' of https://dev.azure.com/inatech/Shiptech10/_git/Shiptech10-UI; branch: master ***NO_CI***
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C30632

--- v2/shiptech/libs/feature/smart/src/lib/shared/pipes/groupByParticipantPipe/group-by-participant.pipe.ts
+++ v2/shiptech/libs/feature/smart/src/lib/shared/pipes/groupByParticipantPipe/group-by-participant.pipe.ts
@@ -7,8 +7,14 @@ export class GroupByParticipantPipe implements PipeTransform {
 
   transform(DataList: any[], args: any, searchText: string): any {
     args = args.split('.');
+    let DuplicateFilterArr = [];
     if(searchText && searchText.trim()) {
-      return DataList.filter(item => (((item[args[0]][args[1]]).toLowerCase()).indexOf((searchText.toLowerCase()))>-1));
+      return DataList.filter(item => {
+        if(!(DuplicateFilterArr.includes(item[args[0]][args[1]]))){
+          DuplicateFilterArr.push(item[args[0]][args[1]]);
+          return (((item[args[0]][args[1]]).toLowerCase()).indexOf((searchText.toLowerCase()))>-1)
+        }
+      });
     } else {
       return DataList;
     }
