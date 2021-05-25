3f48737f1b34ac35bd0e2094ea3148dd01ef9d4a
udaya kumar s <udayakumar.s@inatech.com>
2021-05-25 13:48:24 +0000

    Merged PR 10931: Bug 32519: Comment section-Participants names are displaying incorrectly and Bug 32526: Comment section-Clear filter by participant functionality is not working issue has fixed
    Related work items: #32519, #32526

--- ShiptechV2/libs/feature/smart/src/lib/shared/pipes/groupByParticipantPipe/group-by-participant.pipe.ts
+++ ShiptechV2/libs/feature/smart/src/lib/shared/pipes/groupByParticipantPipe/group-by-participant.pipe.ts
@@ -5,14 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class GroupByParticipantPipe implements PipeTransform {
 
-  transform(DataList: any[], args: any, searchText: string): any {
+  transform(DataList: any[], searchText: string, args: any, args2: any): any {
     args = args.split('.');
     let DuplicateFilterArr = [];
     if(searchText && searchText.trim()) {
       return DataList.filter(item => {
         if(!(DuplicateFilterArr.includes(item[args[0]][args[1]]))){
           DuplicateFilterArr.push(item[args[0]][args[1]]);
-          return (((item[args[0]][args[1]]).toLowerCase()).indexOf((searchText.toLowerCase()))>-1)
+          return (((item[args[0]][args[1]]).toLowerCase()).indexOf((searchText.toLowerCase()))>-1) || (((item[args2]).toLowerCase()).indexOf((searchText.toLowerCase()))>-1)
         }
       });
     } else {
