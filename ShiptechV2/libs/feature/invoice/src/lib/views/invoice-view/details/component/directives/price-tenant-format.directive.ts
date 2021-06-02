febd825e945cf7466d8bead86380718e32ddf3ee
Florin Marin <marinflorinalexandru@gmail.com>
2021-06-02 18:50:34 +0300

    build fix

--- ShiptechV2/libs/feature/invoice/src/lib/views/invoice-view/details/component/directives/price-tenant-format.directive.ts
+++ ShiptechV2/libs/feature/invoice/src/lib/views/invoice-view/details/component/directives/price-tenant-format.directive.ts
@@ -44,11 +44,11 @@ export class PriceTenantFormatDirective implements OnInit {
         this.el.value = plainNumber;
       } else{
           var numberPrecision = this.tenantService.pricePrecision;
-          if(this.el.attributes.precision) {
-              if(this.el.attributes.precision.value !== null) {
-                numberPrecision = this.el.attributes.precision.value;
-              }
-          }
+        //   if(this.el.attributes.precision) {
+        //       if(this.el.attributes.precision.value !== null) {
+        //         numberPrecision = this.el.attributes.precision.value;
+        //       }
+        //   }
         this.el.value = this._decimalPipe.transform(plainNumber, '1.' + numberPrecision + '-' + numberPrecision);
       }
     }
