0fa62de2c9a78d7fcd2ef7eeb87368b6d7cee98b
udaya kumar s <udayakumar.s@inatech.com>
2021-06-07 13:01:14 +0000

    Merged PR 11297: Bug 32814: Current Bunkering plan: HSFO label
    Date format custom pipe has created and formatted by using ng date format
    
    Related work items: #32814

---
+++ ShiptechV2/libs/feature/smart/src/lib/shared/pipes/EnUsDatePipe/en-us-date-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { EnUsDatePipePipe } from './en-us-date-pipe.pipe';
+
+describe('EnUsDatePipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new EnUsDatePipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
