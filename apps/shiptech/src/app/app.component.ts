56d113831313eba39f2dc84706535c145d649861
Avram Ioana <Ioana.Avram@24software.ro>
2021-06-09 17:30:25 +0300

    Removed sematext in v2

--- apps/shiptech/src/app/app.component.ts
+++ apps/shiptech/src/app/app.component.ts
@@ -35,10 +35,13 @@ export class AppComponent {
   loading: boolean;
   firstApiCallStartTime: any;
 
-  constructor(private router: Router, changeDetector: ChangeDetectorRef, 
-              private myMonitoringService: MyMonitoringService, 
-              private loaderService: LoaderService,
-              private elementRef : ElementRef) {
+  constructor(
+    private router: Router,
+    changeDetector: ChangeDetectorRef,
+    private myMonitoringService: MyMonitoringService,
+    private loaderService: LoaderService,
+    private elementRef: ElementRef
+  ) {
     router.events.subscribe((event: RouterEvent): void => {
       if (
         event instanceof NavigationEnd ||
@@ -49,35 +52,35 @@ export class AppComponent {
         changeDetector.markForCheck();
       }
       if (event instanceof NavigationStart) {
-        window['strum']('routeChange', event.url);
       }
     });
-    this.loaderService.isLoading.subscribe((v) => {
+    this.loaderService.isLoading.subscribe(v => {
       if (v) {
         console.log('SEND VALUE');
-        console.log((<any>window).lastCall -   (<any>window).firstCall);
-        if (!isNaN((<any>window).lastCall -   (<any>window).firstCall)) {
-          this.myMonitoringService.logMetric(window.location.href, (<any>window).lastCall -   (<any>window).firstCall, window.location);
+        console.log((<any>window).lastCall - (<any>window).firstCall);
+        if (!isNaN((<any>window).lastCall - (<any>window).firstCall)) {
+          this.myMonitoringService.logMetric(
+            window.location.href,
+            (<any>window).lastCall - (<any>window).firstCall,
+            window.location
+          );
         }
         delete (<any>window).firstCall;
         delete (<any>window).lastCall;
         delete (<any>window).visibleLoader;
         delete (<any>window).openedScreenLoaders;
       }
-
     });
   }
 
   @HostListener('document:click', ['$event.target'])
-    public onClick(targetElement) {
-      let array = ['Documents', 'Audit Log', 'Email Log', 'Main Page'];
-      let findElement = array.find(function(element) {
-        return element == targetElement.innerText;
-      });
-      if (findElement) {
-        delete (<any>window).openedScreenLoaders;
-      }
+  public onClick(targetElement) {
+    let array = ['Documents', 'Audit Log', 'Email Log', 'Main Page'];
+    let findElement = array.find(function(element) {
+      return element == targetElement.innerText;
+    });
+    if (findElement) {
+      delete (<any>window).openedScreenLoaders;
     }
-
-  
+  }
 }
