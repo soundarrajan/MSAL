dba87470ffe691fc7226ede4133a799616990325
Divya Premachandran <divya.p@inatech.com>
2021-06-18 13:35:00 +0000

    Git-2-Tfs Sync. 9f562c0 Merged PR 11664: #29751 : vessel arrivals req; branch: master ***NO_CI***
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C31068

--- v2/shiptech/libs/feature/smart/src/lib/shared/port-info/port-info.component.ts
+++ v2/shiptech/libs/feature/smart/src/lib/shared/port-info/port-info.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { CommentsComponent } from '../comments/comments.component';
 import { GridOptions } from '@ag-grid-community/core';
 import { AGGridCellRendererComponent } from '../ag-grid/ag-grid-cell-renderer.component';
 import { AGGridCellDataComponent } from '../ag-grid/ag-grid-celldata.component';
+import { PortPopupService } from '../../services/port-popup.service';
 
 @Component({
   selector: 'app-port-info',
@@ -12,12 +13,13 @@ import { AGGridCellDataComponent } from '../ag-grid/ag-grid-celldata.component';
 export class PortInfoComponent implements OnInit {
 
   @ViewChild(CommentsComponent) child;
+  @Input('portData') portData;
   public expandComments: boolean = false;
   public gridOptions: GridOptions;
   public colResizeDefault;
   public rowCount: Number;
 
-  constructor() {
+  constructor( private portService : PortPopupService ) {
     this.gridOptions = <GridOptions>{
       columnDefs: this.columnDefs,
       //enableColResize: true,
@@ -52,12 +54,30 @@ export class PortInfoComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.loadVesselArrivalDetails(this.portData.locationId);
   }
 
+  loadVesselArrivalDetails(locationId){
+    if(locationId != null){
+      let req = { LocationId : 37}//locationId};
+      this.portService.getVesselArrivalDetails(req).subscribe((res: any)=>{
+        if(res.payload.length > 0){
+          this.rowData = res.payload;
+         
+        }
+        this.triggerClickEvent();
+      })
+    }
+
+  }
   toggleComments(event) {
     event.stopPropagation();
     this.expandComments = !this.expandComments;
     this.child.toggleExpanded();
+  }
+  triggerClickEvent() {
+    let titleEle = document.getElementsByClassName('page-title')[0] as HTMLElement;
+      titleEle.click();
   }
   private columnDefs = [
     // { headerName: 'ETA', headerTooltip: 'ETA', field: 'eta', cellRendererFramework: AGGridCellRendererComponent, cellRendererParams: { cellClass: ['custom-chip dark aggrid-space'] }, headerClass: ['aggrid-text-align-c'], cellClass: ['aggrid-content-center'], width: 140 },
@@ -70,16 +90,16 @@ export class PortInfoComponent implements OnInit {
 
   ];
 
-  private rowData = [
-    { vesselName: "EMMA MAERSK", eta: "2020-04-19 13:00", etd: "2020-04-19 13:00", etaDays: "-", etdDays: "-" },
-    { vesselName: "MCC KYOTO", eta: "2020-04-19 13:00", etd: "2020-04-19 13:00", etaDays: "-", etdDays: "-" },
-    { vesselName: "GUATEMALA", eta: "2020-04-19 13:00", etd: "2020-04-19 09:00", etaDays: "2 Days", etdDays: "2 Days" },
-    { vesselName: "MARCO POLO", eta: "2020-04-19 04:00", etd: "2020-04-19 13:00", etaDays: "3 Days", etdDays: "3 Days" },
-    { vesselName: "ARIANA", eta: "2020-04-19 13:00", etd: "2020-04-19 13:00", etaDays: "3 Days", etdDays: "3 Days" },
-    { vesselName: "MANILA MAERSK", eta: "2020-04-19 13:00", etd: "2020-04-19 13:00", etaDays: "4 Days", etdDays: "4 Days" },
-    { vesselName: "MAERSK ARIZONA", eta: "2020-04-19 13:00", etd: "2020-04-19 21:00", etaDays: "5 Days", etdDays: "5 Days" },
-    { vesselName: "MURCIA MAERSK", eta: "2020-04-19 10:00", etd: "2020-04-19 23:00", etaDays: "6 Days", etdDays: "6 Days" },
-    { vesselName: "MOSCOW MAERSK", eta: "2020-04-19 13:00", etd: "2020-04-19 18:00", etaDays: "7 Days", etdDays: "7 Days" },
-    { vesselName: "CADIZ MAERSK", eta: "2020-04-19 13:00", etd: "2020-04-19 17:00", etaDays: "8 Days", etdDays: "8 Days" }
+  public rowData = [
+    // { vesselName: "EMMA MAERSK", eta: "2020-04-19 13:00", etd: "2020-04-19 13:00", etaDays: "-", etdDays: "-" },
+    // { vesselName: "MCC KYOTO", eta: "2020-04-19 13:00", etd: "2020-04-19 13:00", etaDays: "-", etdDays: "-" },
+    // { vesselName: "GUATEMALA", eta: "2020-04-19 13:00", etd: "2020-04-19 09:00", etaDays: "2 Days", etdDays: "2 Days" },
+    // { vesselName: "MARCO POLO", eta: "2020-04-19 04:00", etd: "2020-04-19 13:00", etaDays: "3 Days", etdDays: "3 Days" },
+    // { vesselName: "ARIANA", eta: "2020-04-19 13:00", etd: "2020-04-19 13:00", etaDays: "3 Days", etdDays: "3 Days" },
+    // { vesselName: "MANILA MAERSK", eta: "2020-04-19 13:00", etd: "2020-04-19 13:00", etaDays: "4 Days", etdDays: "4 Days" },
+    // { vesselName: "MAERSK ARIZONA", eta: "2020-04-19 13:00", etd: "2020-04-19 21:00", etaDays: "5 Days", etdDays: "5 Days" },
+    // { vesselName: "MURCIA MAERSK", eta: "2020-04-19 10:00", etd: "2020-04-19 23:00", etaDays: "6 Days", etdDays: "6 Days" },
+    // { vesselName: "MOSCOW MAERSK", eta: "2020-04-19 13:00", etd: "2020-04-19 18:00", etaDays: "7 Days", etdDays: "7 Days" },
+    // { vesselName: "CADIZ MAERSK", eta: "2020-04-19 13:00", etd: "2020-04-19 17:00", etaDays: "8 Days", etdDays: "8 Days" }
   ];
 }
