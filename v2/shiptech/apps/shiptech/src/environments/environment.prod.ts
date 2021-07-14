87d155896fd07cf8cb4895c8888d1955e4e039af
Anurag Dwivedi <anurag.d@inatech.com>
2021-07-14 07:14:15 +0000

    Git-2-Tfs Sync. 6548c4d Merged PR 12374: View Notifications; branch: master ***NO_CI***
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C31585

--- v2/shiptech/apps/shiptech/src/environments/environment.prod.ts
+++ v2/shiptech/apps/shiptech/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  instrumentationKey: '61f82289-6395-460f-81ca-9cb0da7407b2'
+  instrumentationKey: '61f82289-6395-460f-81ca-9cb0da7407b2',
+  firebase:{
+    apiKey: "AIzaSyCpbhEpU4urC9CK_9PICTvU6q2C2MVcUGU",
+    authDomain: "inatechai-cfrxtm.firebaseapp.com",
+    databaseURL: "https://inatechai-cfrxtm.firebaseio.com",
+    projectId: "inatechai-cfrxtm",
+    storageBucket: "inatechai-cfrxtm.appspot.com",
+    messagingSenderId: "122257773907",
+    appId: "1:122257773907:web:932851ef6995ae89"
+    }
 };
---
+++ v2/shiptech/apps/shiptech/src/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+  importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+  importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+
+      
+  firebase.initializeApp({
+    'messagingSenderId': '122257773907'
+  });
+    
+    const messaging = firebase.messaging(); 
+// 
+
+
