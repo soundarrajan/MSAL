e7f2dec011f4def4ef169cd619959ab2057b14c2
Florin Marin <marinflorinalexandru@gmail.com>
2021-08-16 18:15:29 +0300

    Fix 2 invoice tabs

--- ShiptechV2/libs/feature/invoice/src/lib/views/invoice-view/toolbar/invoice-details-toolbar.component.ts
+++ ShiptechV2/libs/feature/invoice/src/lib/views/invoice-view/toolbar/invoice-details-toolbar.component.ts
@@ -45,7 +45,7 @@ export class InvoiceDetailsToolbarComponent
     }
     this.navBar = navBar;
     let params = {
-      invoiceId: this.invoiceId
+      invoiceId: this.navBar.invoiceId ? this.navBar.invoiceId : this.invoiceId
     };
     this.setNavIds(params, this.navBar);
     this.createNavigationItems(this.navBar);
