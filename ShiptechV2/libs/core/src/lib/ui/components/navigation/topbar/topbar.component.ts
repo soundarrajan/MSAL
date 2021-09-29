3001d0a808b883d398eb9a4c15bcc4ff084a1b78
Avram Ioana <Ioana.Avram@24software.ro>
2021-09-29 15:43:31 +0300

    Revert msal_library

--- ShiptechV2/libs/core/src/lib/ui/components/navigation/topbar/topbar.component.ts
+++ ShiptechV2/libs/core/src/lib/ui/components/navigation/topbar/topbar.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LayoutMainComponent } from '../../../layout/main/layout-main.component';
+import { AuthenticationService } from '../../../../authentication/authentication.service';
 import { Observable } from 'rxjs';
 import { Select } from '@ngxs/store';
 import { UserProfileState } from '@shiptech/core/store/states/user-profile/user-profile.state';
 import { TitleService } from '@shiptech/core/services/title/title.service';
-import { AuthService } from '@shiptech/core/authentication/auth.service';
 
 @Component({
   selector: 'shiptech-topbar',
@@ -19,7 +19,7 @@ export class TopbarComponent {
 
   constructor(
     public app: LayoutMainComponent,
-    public authService: AuthService,
+    public authService: AuthenticationService,
     titleService: TitleService
   ) {
     this.pageTitle$ = titleService.title$;
