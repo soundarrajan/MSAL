3d58931d7c2b099de7a6dccec7a2cd3869bbcffe
udayakumar.s@Inatech.com <udayakumar.s@Inatech.com>
2021-10-21 11:03:34 +0000

    Git-2-Tfs Sync. 83dbb43 Merged PR 14227: #33800: Admin: User Screen UI changes has done; branch: master ***NO_CI***
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C32740

--- v2/shiptech/libs/feature/control-tower/src/lib/views/control-tower/view/components/control-tower-home-new/control-tower-home-new.component.ts
+++ v2/shiptech/libs/feature/control-tower/src/lib/views/control-tower/view/components/control-tower-home-new/control-tower-home-new.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-control-tower-home-new',
   templateUrl: './control-tower-home-new.component.html',
   styleUrls: ['./control-tower-home-new.component.css']
 })
-export class ControlTowerHomeNewComponent implements OnInit {
+export class ControlTowerHomeNewComponent implements OnInit, AfterViewInit {
+  @ViewChild('controlViewElem') controlViewRef!: MatSelectChange;
   public showQuality: boolean = false;
   public showQuantity: boolean = true;
   public showResidue: boolean = false;
   public theme;
   public newScreen = true;
-  selected = 'quantity';
+  selected:string;
 
   selectedVal: string = 'labs';
   selectedVal2: string = 'differences';
   selectedVal3: string = 'differences';
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {
+    //load default landing page screen based on user preference
+    this.loadDefaultLandingPage();
+  }
 
   ngOnInit(): void {
     /* this.localService.themeChange.subscribe(data => {
@@ -25,10 +31,38 @@ export class ControlTowerHomeNewComponent implements OnInit {
     }) */
   }
 
+  ngAfterViewInit() {
+    this.viewChange(this.controlViewRef);
+  }
+  
   public onValChange(val: string) {
     this.selectedVal = val;
     this.selectedVal2 = val;
     this.selectedVal3 = val;
+  }
+  
+  loadDefaultLandingPage() {
+    let selectedId = this.route.snapshot.paramMap.get('id');
+    // load default landing screen based on quality, quantity, residue view
+    if(selectedId) {
+      switch (Number(selectedId)) {
+        case 5:
+          this.selected = 'quality';
+          break;
+          case 6:
+            this.selected = 'quantity';
+            break;
+            case 7:
+              this.selected = 'residue';
+              break;
+              default:
+                this.selected = 'quantity';
+                break;
+              }
+    } else {
+      // load default landing screen quantity view, if there is no default view based on user
+      this.selected = 'quantity';
+    }
   }
 
   viewChange($event) {
