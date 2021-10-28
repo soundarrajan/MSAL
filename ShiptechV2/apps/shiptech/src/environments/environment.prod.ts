565640e2e9344092681499cee89a0f572d3d1c77
Avram Ioana <Ioana.Avram@24software.ro>
2021-10-28 13:09:34 +0300

    Worked on 36542

--- ShiptechV2/apps/shiptech/src/environments/environment.prod.ts
+++ ShiptechV2/apps/shiptech/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
-export const environment = {
+export let environment = {
   production: false,
   instrumentationKey: '61f82289-6395-460f-81ca-9cb0da7407b2',
-  useAdal: window.location.hostname.indexOf('cma') != -1 ? false : true
+  useAdal: window.location.hostname.indexOf('cma') != -1 ? true : false
 };
