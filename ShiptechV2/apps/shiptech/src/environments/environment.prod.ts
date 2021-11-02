655fe8d670e7418ae515df44426952920e845011
Edwin Francis <edwin.f@Inatech.com>
2021-11-02 06:48:17 +0000

    Updated environment.prod.ts

--- ShiptechV2/apps/shiptech/src/environments/environment.prod.ts
+++ ShiptechV2/apps/shiptech/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export let environment = {
   production: false,
   instrumentationKey: '61f82289-6395-460f-81ca-9cb0da7407b2',
-  useAdal: window.location.hostname.indexOf('pmg') != -1 ? true : false
+  useAdal: (window.location.hostname.indexOf('cma') != -1 || window.location.hostname.indexOf('pmg') != -1) ? true : false
 };
