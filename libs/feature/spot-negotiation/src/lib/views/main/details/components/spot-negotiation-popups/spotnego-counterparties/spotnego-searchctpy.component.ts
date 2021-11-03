623a6f569d17e7bee642b8db2c406363205261be
Edwin Francis <edwin.f@Inatech.com>
2021-11-03 17:28:53 +0530

    Confirm order popup design fixes, on price change event update and seller selection as from backend api.

--- libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/spotnego-counterparties/spotnego-searchctpy.component.ts
+++ libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/spotnego-counterparties/spotnego-searchctpy.component.ts
@@ -222,7 +222,7 @@ export class SpotnegoSearchCtpyComponent implements OnInit {
           "phySupplierId": selectedCounterparties[0].sellerCounterpartyId
       }
 
-      const response = this._spotNegotiationService.updatePhySupplier(reqPayload);
+    const response = this._spotNegotiationService.updatePhySupplier(reqPayload);
     response.subscribe((res: any) => {
       if (res.status) {
         this.toastr.success(res.message);
