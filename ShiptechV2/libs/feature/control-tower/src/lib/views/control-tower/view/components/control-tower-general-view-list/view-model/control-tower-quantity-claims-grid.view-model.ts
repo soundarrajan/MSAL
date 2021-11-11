21958cb188054fd536b851559e2b3029fc09d8d5
Avram Ioana <Ioana.Avram@24software.ro>
2021-11-11 09:07:16 +0200

    Loaded last 7 days by default in quantity difference list

--- ShiptechV2/libs/feature/control-tower/src/lib/views/control-tower/view/components/control-tower-general-view-list/view-model/control-tower-quantity-claims-grid.view-model.ts
+++ ShiptechV2/libs/feature/control-tower/src/lib/views/control-tower/view/components/control-tower-general-view-list/view-model/control-tower-quantity-claims-grid.view-model.ts
@@ -55,7 +55,11 @@ export class ControlTowerQuantityClaimsListGridViewModel extends BaseGridViewMod
       .subtract(6, 'months')
       .format('YYYY-MM-DD')
   );
-  public toDate = new FormControl(moment().format('YYYY-MM-DD'));
+  public toDate = new FormControl(
+    moment()
+      .subtract(1, 'days')
+      .format('YYYY-MM-DD')
+  );
 
   public toggleNewFilter: boolean = true;
   public toggle714DaysFilter: boolean = true;
