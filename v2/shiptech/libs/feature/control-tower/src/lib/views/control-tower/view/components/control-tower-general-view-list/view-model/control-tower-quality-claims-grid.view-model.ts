8708eb9b98b8d03d96cf6e619afc62bd6e7797af
Florin-Alexandru.Marin@24software.ro <Florin-Alexandru.Marin@24software.ro>
2021-11-11 16:43:43 +0000

    Git-2-Tfs Sync. ca1470d Merged PR 14671: changed control tower quality claim mapping; branch: master ***NO_CI***
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C32963

--- v2/shiptech/libs/feature/control-tower/src/lib/views/control-tower/view/components/control-tower-general-view-list/view-model/control-tower-quality-claims-grid.view-model.ts
+++ v2/shiptech/libs/feature/control-tower/src/lib/views/control-tower/view/components/control-tower-general-view-list/view-model/control-tower-quality-claims-grid.view-model.ts
@@ -216,7 +216,7 @@ export class ControlTowerQualityClaimsListGridViewModel extends BaseGridViewMode
     headerName: ControlTowerQualityClaimsListColumnsLabels.claimSubType,
     headerTooltip: ControlTowerQualityClaimsListColumnsLabels.claimSubType,
     colId: ControlTowerQualityClaimsListColumns.claimSubType,
-    field: model('claimSubType'),
+    field: model('claimSubTypes'),
     valueFormatter: params => this.format.htmlDecode(params.value),
     dtoForExport: ControlTowerQualityClaimsListExportColumns.claimSubType,
     tooltip: params => (params.value ? this.format.htmlDecode(params.value) : ''),
