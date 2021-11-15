0cb97f7fa696725823537f56a478c0f7b01ddfda
Avram Ioana <Ioana.Avram@24software.ro>
2021-11-15 11:46:49 +0200

    Fixed refreshing the quantity view page(36890)

--- ShiptechV2/libs/feature/control-tower/src/lib/control-tower.routes.ts
+++ ShiptechV2/libs/feature/control-tower/src/lib/control-tower.routes.ts
@@ -1,11 +1,3 @@
 export enum KnownControlTowerRoutes {
-  ReportList = 'reports-list',
-  ControlTowerList = 'control-tower-list-view',
-  ControlTowerInvoiceList = 'control-tower-invoice-list',
-  Report = 'report',
-  ReportDetails = 'details',
-  ReportIdParam = 'reportId',
-  ReportAuditPath = 'audit-log',
-  ReportDocumentsPath = 'documents',
-  ReportEmailLogPath = 'email-log'
+  ControlTowerList = 'control-tower-list-view'
 }
