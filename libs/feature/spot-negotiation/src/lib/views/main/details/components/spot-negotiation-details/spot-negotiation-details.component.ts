dd999ee500489d35a97e4283b4532880867e307c
Prabaharan <prabaharan.p@Inatech.com>
2021-11-16 17:50:06 +0530

    phySupplierId is default null fix#updatepriec

--- libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-details/spot-negotiation-details.component.ts
+++ libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-details/spot-negotiation-details.component.ts
@@ -319,7 +319,7 @@ export class SpotNegotiationDetailsComponent implements OnInit {
       return;
     }
     const payload = {
-      phySupplierId : null,
+      phySupplierId : updatedRow.physicalSupplierCounterpartyId,
       Offers: [
         {
           id: productDetails.offerId,
