12ee2c22585ff3ec2211efe8ef383f0e1ace8714
Avram Ioana <Ioana.Avram@24software.ro>
2021-11-17 15:04:52 +0200

    Worked on my notes

--- ShiptechV2/libs/feature/control-tower/src/lib/services/api/dto/control-tower-list-item.dto.ts
+++ ShiptechV2/libs/feature/control-tower/src/lib/services/api/dto/control-tower-list-item.dto.ts
@@ -35,6 +35,7 @@ export interface IControlTowerQuantitySupplyDifferenceItemDto {
   port: IDisplayLookupDto;
   vessel: IDisplayLookupDto;
   eta: string;
+  id: number;
   surveyorDate: string;
   emailToVessel: boolean;
   vesselToWatch: boolean;
