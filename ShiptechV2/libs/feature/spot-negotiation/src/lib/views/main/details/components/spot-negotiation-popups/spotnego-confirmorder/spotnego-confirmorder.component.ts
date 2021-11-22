e8b0cca05ffc514d6c7b5ce5c21ec9c07d49fe63
Prabaharan <prabaharan.p@Inatech.com>
2021-11-22 21:54:53 +0530

    #37117:Multiple Order IDs displayed & #37118:Unable to create 2nd Order with a new Seller both issue fix

--- ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/spotnego-confirmorder/spotnego-confirmorder.component.ts
+++ ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/spotnego-confirmorder/spotnego-confirmorder.component.ts
@@ -328,7 +328,7 @@ export class SpotnegoConfirmorderComponent implements OnInit {
           this.responseOrderData.forEach((rodV ,rodK ) => {
             hasError = false;
             rodV.products.forEach((rodProdV,rodProdK ) => {
-              if (rodV.requestLocationId == rqV.RequestLocationId /*&& rodProdV.requestProductId == rqV.RequestProductId*/) {
+              if (rodV.requestLocationId == rqV.RequestLocationId && rodProdV.requestProductId == rqV.RequestProductId) {
                 hasOrder = true;
                 let errorType = [];
                 if (rodV.seller.id != rqV.SellerId) {
@@ -405,7 +405,7 @@ export class SpotnegoConfirmorderComponent implements OnInit {
           if(res instanceof Object && res.payload.length > 0 ){
             //this.openEditOrder(receivedOffers.payload);
             const baseOrigin = new URL(window.location.href).origin;
-            window.open(`${ baseOrigin }/#/edit-order/${receivedOffers.payload}`, '_blank');
+            window.open(`${ baseOrigin }/#/edit-order/${receivedOffers.payload[0]}`, '_blank');
             this.toaster.success('order created successfully.')
           }
           else if(res instanceof Object){
