525b694fd75dce66407336494bca738848f4a2dd
Nagarajan <nagarajan.m@Inatech.com>
2022-01-07 18:38:45 +0530

    #37734 - updated code to handel skipped product while revoking, amend and requote

--- ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-home/spot-negotiation-home.component.ts
+++ ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-home/spot-negotiation-home.component.ts
@@ -391,7 +391,7 @@ export class SpotNegotiationHomeComponent implements OnInit {
       this.toaster.error('Amend RFQ cannot be sent as RFQ was not communicated.');
       return;
     }
-    else if (this.selectedSellerList.find(x => x.RfqId !== 0 && x.RequestOffers?.find(x => x.isRfqskipped === true))) {
+    else if (this.selectedSellerList.find(x => x.RfqId !== 0 && x.RequestOffers?.filter(x => !x.isRfqskipped && !x.isDeleted).length === 0)) {
       this.toaster.error('Amended RFQ cannot be sent as RFQ was skipped.');
       return;
     }
@@ -494,7 +494,7 @@ export class SpotNegotiationHomeComponent implements OnInit {
       this.toaster.error('Revoke RFQ cannot be sent as RFQ was not communicated.');
       return;
     }
-    else if (this.selectedSellerList.find(x => x.RfqId !== 0 && x.RequestOffers?.find(x => x.isRfqskipped === true))) {
+    else if (this.selectedSellerList.find(x => x.RfqId !== 0 && x.RequestOffers?.filter(x => !x.isRfqskipped && !x.isDeleted).length === 0)) {
       this.toaster.error('Revoke RFQ cannot be sent as RFQ was skipped.');
       return;
     }
@@ -562,7 +562,7 @@ export class SpotNegotiationHomeComponent implements OnInit {
       this.toaster.error('Requote RFQ cannot be sent as RFQ was not communicated.');
       return;
     }
-    else if (this.selectedSellerList.find(x => x.RfqId !== 0 && x.RequestOffers?.find(x => x.isRfqskipped === true))) {
+    else if (this.selectedSellerList.find(x => x.RfqId !== 0 && x.RequestOffers?.filter(x => !x.isRfqskipped && !x.isDeleted).length === 0)) {
       this.toaster.error('Requote RFQ cannot be sent as RFQ was skipped.');
       return;
     }
