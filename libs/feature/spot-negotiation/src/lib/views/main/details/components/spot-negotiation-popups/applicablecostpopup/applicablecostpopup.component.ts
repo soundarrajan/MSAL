6e6e623081d236510f8074f01d6ec6e00d56481f
Avram Ioana <Ioana.Avram@24software.ro>
2022-01-14 14:33:14 +0200

    Added calculations in view location cost pop-up(33819)

--- libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/applicablecostpopup/applicablecostpopup.component.ts
+++ libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/applicablecostpopup/applicablecostpopup.component.ts
@@ -18,6 +18,21 @@ import { TenantSettingsService } from '@shiptech/core/services/tenant-settings/t
 import { TenantFormattingService } from '@shiptech/core/services/formatting/tenant-formatting.service';
 import { DecimalPipe } from '@angular/common';
 import { IGeneralTenantSettings } from '@shiptech/core/services/tenant-settings/general-tenant-settings.interface';
+import { finalize } from 'rxjs/operators';
+
+export const COMPONENT_TYPE_IDS = {
+  TAX_COMPONENT: 1,
+  PRODUCT_COMPONENT: 2
+};
+
+export const COST_TYPE_IDS = {
+  FLAT: 1,
+  UNIT: 2,
+  PERCENT: 3,
+  RANGE: 4,
+  TOTAL: 5
+};
+
 @Component({
   selector: 'app-applicablecostpopup',
   templateUrl: './applicablecostpopup.component.html',
@@ -48,12 +63,18 @@ export class ApplicablecostpopupComponent implements OnInit {
   quantityFormat: string;
   amountFormat: string;
   priceFormat: string;
+  productList: any = [];
+  additionalCostList: any = [];
+  additionalCostTypes: any = [];
+  requestLocation: any;
+  sellers: any;
+  saveButtonClicked: boolean = false;
 
   constructor(
     public dialogRef: MatDialogRef<ApplicablecostpopupComponent>,
     private spinner: NgxSpinnerService,
     private toastr: ToastrService,
-    @Inject(MAT_DIALOG_DATA) public requestLocation: any,
+    @Inject(MAT_DIALOG_DATA) public data: any,
     private spotNegotiationService: SpotNegotiationService,
     private legacyLookupsDatabase: LegacyLookupsDatabase,
     private tenantSettingsService: TenantSettingsService,
@@ -79,6 +100,9 @@ export class ApplicablecostpopupComponent implements OnInit {
       this.tenantService.pricePrecision +
       '-' +
       this.tenantService.pricePrecision;
+
+    this.requestLocation = data.reqLocation;
+    this.sellers = data.sellers;
   }
 
   ngOnInit() {
@@ -108,11 +132,43 @@ export class ApplicablecostpopupComponent implements OnInit {
             );
           });
       });
+
+    this.spotNegotiationService
+      .getMasterAdditionalCosts({})
+      .subscribe((response: any) => {
+        if (typeof response === 'string') {
+          this.toastr.error(response);
+        } else {
+          this.additionalCostList = _.cloneDeep(response.payload);
+          this.createAdditionalCostTypes();
+        }
+      });
+  }
+
+  createAdditionalCostTypes() {
+    for (let i = 0; i < this.additionalCostList.length; i++) {
+      if (
+        typeof this.additionalCostTypes[this.additionalCostList[i].id] ==
+        'undefined'
+      ) {
+        this.additionalCostTypes[this.additionalCostList[i].id] = [];
+      }
+      this.additionalCostTypes[
+        this.additionalCostList[i].id
+      ] = this.additionalCostList[i];
+    }
   }
 
   saveLocationAdditionalCosts() {
     if (!this.enableSave) {
       this.toastr.warning('No changes are made to perform save.');
+      return;
+    }
+
+    this.saveButtonClicked = true;
+    let locationBasedCostsRequiredString = this.checkRequiredFields();
+    if (locationBasedCostsRequiredString != '') {
+      this.toastr.error(locationBasedCostsRequiredString);
       return;
     }
 
@@ -128,11 +184,9 @@ export class ApplicablecostpopupComponent implements OnInit {
             res?.costs?.locationAdditionalCosts
           );
           this.toastr.success('Additional cost saved successfully.');
-          this.deletedCosts = [];
+          this.enableSave = false;
         } else this.toastr.error('Please try again later.');
       });
-
-    this.enableSave = false;
   }
 
   formatCostItemForDisplay(locationAdditionalCosts: any) {
@@ -143,31 +197,77 @@ export class ApplicablecostpopupComponent implements OnInit {
       cost.costType = this.costTypeList.find(
         c => c.id === cost.costTypeId
       )?.name;
-      cost.maxQuantityUom = this.uomList.find(
-        c => c.id === cost.maxQuantityUomId
-      )?.name;
-      cost.currency = this.currencyList.find(
-        c => c.id === cost.currencyId
-      )?.code;
+
+      this.checkIfLineIsApplicableToStemmedProduct(cost);
     });
 
     return locationAdditionalCosts;
   }
 
+  checkIfLineIsApplicableToStemmedProduct(locationAdditionalCost) {
+    let findProductIndex = _.findIndex(
+      this.requestLocation.requestProducts,
+      function(product: any) {
+        return (
+          product.id == locationAdditionalCost.requestProductId &&
+          product.status == 'Stemmed'
+        );
+      }
+    );
+    if (findProductIndex != -1) {
+      locationAdditionalCost.hasStemmedProduct = true;
+      locationAdditionalCost.product = {
+        name: this.requestLocation.requestProducts[findProductIndex]
+          .productName,
+        id: this.requestLocation.requestProducts[findProductIndex].productId
+      };
+    }
+  }
+
+  checkIfSelectedApplicableIdExistsInapplicableForItems(
+    locationAdditionalCost
+  ) {
+    let findIndex = _.findIndex(this.applicableForItems, function(object: any) {
+      return object.id == locationAdditionalCost.selectedApplicableForId;
+    });
+    if (
+      findIndex == -1 &&
+      locationAdditionalCost.id &&
+      !locationAdditionalCost.selectedApplicableForId
+    ) {
+      return false;
+    }
+    return true;
+  }
+
   buildApplicableForItems() {
-    this.applicableForItems.push({ id: 0, name: 'All' });
+    let applicableForItemsArray = [];
     this.requestLocation.requestProducts.forEach((product: any) => {
       if (product.status !== 'Stemmed') {
-        this.applicableForItems.push({
+        applicableForItemsArray.push({
           id: product.id,
           name: product.productName,
           productId: product.productId
         });
+
+        this.productList.push(product);
+
         this.totalMaxQuantity = this.totalMaxQuantity + product.maxQuantity;
         this.maxQuantityUomId = product.uomId;
         this.maxQuantityUom = product.uomName;
       }
     });
+
+    if (applicableForItemsArray.length > 1) {
+      const allElement = { id: 0, name: 'All' };
+      this.applicableForItems = _.cloneDeep(
+        [allElement].concat(applicableForItemsArray)
+      );
+    } else {
+      this.applicableForItems = _.cloneDeep(applicableForItemsArray);
+    }
+
+    console.log(this.applicableForItems);
 
     this.legacyLookupsDatabase.getTableByName('costType').then(response => {
       this.costTypeList = response;
@@ -202,12 +302,19 @@ export class ApplicablecostpopupComponent implements OnInit {
     cost.maxQuantity = maxQtyDetails.maxQty;
     cost.maxQuantityUomId = maxQtyDetails.maxQtyUomId;
 
-    cost.priceUomId = maxQtyDetails.maxQtyUomId;
+    cost.priceUomId = selectedCost.priceUomId;
     cost.price = selectedCost.price;
 
     cost.currencyId = selectedCost.currencyId;
     cost.extras = selectedCost.extrasPercentage;
-    this.calculateCostAmount(cost);
+
+    // Check if selected cost type is equal with Unit
+    if (cost?.costTypeId == 2) {
+      this.addPriceUomChanged(cost);
+    } else {
+      this.calculateCostAmount(cost);
+      this.recalculatePercentAdditionalCosts(this.locationBasedCosts);
+    }
     this.enableSave = true;
   }
 
@@ -244,9 +351,28 @@ export class ApplicablecostpopupComponent implements OnInit {
     cost.maxQuantity = maxQtyDetails.maxQty;
     cost.maxQuantityUom = maxQtyDetails.maxQtyUom;
     cost.maxQuantityUomId = maxQtyDetails.maxQtyUomId;
-    cost.priceUomId = maxQtyDetails.maxQtyUomId;
-    this.calculateCostAmount(cost);
+
+    cost.priceUomId = cost.priceUomId ? cost.priceUomId : null;
+
+    // Check if selected cost type is equal with Unit
+    if (cost?.costTypeId == 2) {
+      this.addPriceUomChanged(cost);
+    } else {
+      this.calculateCostAmount(cost);
+      this.recalculatePercentAdditionalCosts(this.locationBasedCosts);
+    }
     this.enableSave = true;
+  }
+
+  recalculatePercentAdditionalCosts(locationBasedCosts) {
+    for (let i = 0; i < locationBasedCosts.length; i++) {
+      if (!locationBasedCosts[i].isDeleted) {
+        if (locationBasedCosts[i].costTypeId == COST_TYPE_IDS.PERCENT) {
+          locationBasedCosts[i].totalAmount = 0;
+          this.calculateCostAmount(locationBasedCosts[i]);
+        }
+      }
+    }
   }
 
   calculateCostAmount(cost: any) {
@@ -254,19 +380,61 @@ export class ApplicablecostpopupComponent implements OnInit {
     cost.extraAmount = cost.extras ? cost.amount * (cost.extras / 100) : 0;
     cost.totalAmount = cost.amount + cost.extraAmount;
     cost.ratePerUom = cost.totalAmount / cost.maxQuantity;
+    console.log(cost);
   }
 
   getCostAmountByType(cost: any) {
-    var costAmount = 0;
+    let costAmount = 0,
+      productComponent = false;
     switch (cost.costType) {
       case 'Flat':
         costAmount = cost.price;
         break;
       case 'Unit':
-        costAmount = cost.maxQuantity * cost.price;
+        if (
+          cost.priceUomId &&
+          cost.prodConv &&
+          cost.prodConv.length == this.productList.length
+        ) {
+          for (let i = 0; i < this.productList.length; i++) {
+            let product = this.productList[i];
+            if (cost.isAllProductsCost || product.id == cost.requestProductId) {
+              costAmount =
+                costAmount +
+                product.maxQuantity * cost.prodConv[i] * parseFloat(cost.price);
+            }
+          }
+        }
         break;
       case 'Percent':
-        costAmount = cost.price;
+        productComponent = this.isProductComponent(cost);
+        let productAmount = 0;
+        if (cost.isAllProductsCost || !productComponent) {
+          let requestOffers = this.getRequestOffers(this.sellers);
+          productAmount = this.sumProductAmounts(requestOffers);
+        } else {
+          let requestOffers = this.getRequestOffers(this.sellers);
+          let filterRequestOffersByProductId = _.filter(requestOffers, function(
+            offer
+          ) {
+            return offer.requestProductId == cost.requestProductId;
+          });
+          productAmount = this.sumProductAmounts(
+            filterRequestOffersByProductId
+          );
+        }
+        if (productComponent) {
+          costAmount = (productAmount * cost.price) / 100;
+        } else {
+          let taxAmount = this.sumProductComponentAdditionalCostAmounts(
+            this.locationBasedCosts
+          );
+          console.log(productAmount);
+          console.log(taxAmount);
+          let totalAmount = productAmount + taxAmount;
+          costAmount = (totalAmount * cost.price) / 100;
+        }
+        console.log(costAmount);
         break;
       case 'Range':
       case 'Total':
@@ -274,6 +442,71 @@ export class ApplicablecostpopupComponent implements OnInit {
         break;
     }
     return costAmount;
+  }
+
+  addPriceUomChanged(additionalCost) {
+    if (!additionalCost.priceUomId) {
+      return;
+    }
+    additionalCost.prodConv = _.cloneDeep([]);
+
+    for (let i = 0; i < this.productList.length; i++) {
+      let prod = this.productList[i];
+      this.setConvertedAddCost(prod, additionalCost, i);
+    }
+  }
+
+  setConvertedAddCost(prod, additionalCost, i) {
+    this.getConvertedUOM(
+      prod.productId,
+      1,
+      prod.uomId,
+      additionalCost.priceUomId,
+      additionalCost,
+      i
+    );
+  }
+
+  getConvertedUOM(productId, quantity, fromUomId, toUomId, additionalCost, i) {
+    let payload = {
+      Payload: {
+        ProductId: productId,
+        Quantity: quantity,
+        FromUomId: fromUomId,
+        ToUomId: toUomId
+      }
+    };
+
+    if (toUomId == fromUomId) {
+      additionalCost.prodConv[i] = 1;
+      if (
+        additionalCost.priceUomId &&
+        additionalCost.prodConv &&
+        additionalCost.prodConv.length == this.productList.length
+      ) {
+        this.calculateCostAmount(additionalCost);
+        this.recalculatePercentAdditionalCosts(this.locationBasedCosts);
+      }
+    } else {
+      this.spotNegotiationService
+        .getUomConversionFactor(payload)
+        .pipe(finalize(() => {}))
+        .subscribe((result: any) => {
+          if (typeof result == 'string') {
+            this.toastr.error(result);
+          } else {
+            additionalCost.prodConv[i] = _.cloneDeep(result);
+            if (
+              additionalCost.priceUomId &&
+              additionalCost.prodConv &&
+              additionalCost.prodConv.length == this.productList.length
+            ) {
+              this.calculateCostAmount(additionalCost);
+              this.recalculatePercentAdditionalCosts(this.locationBasedCosts);
+            }
+          }
+        });
+    }
   }
 
   getRangeTotalAdditionalCosts(cost: any) {
@@ -323,6 +556,7 @@ export class ApplicablecostpopupComponent implements OnInit {
           this.toastr.error(response);
         } else {
           cost.price = response.price;
+          cost.currencyId = response.currencyId;
           cost.amount = response.price;
 
           cost.extraAmount = cost.extras
@@ -339,6 +573,107 @@ export class ApplicablecostpopupComponent implements OnInit {
       });
   }
 
+  /**
+   * Checks if the given additional cost belongs
+   * to the ProductComponent category.
+   */
+  isProductComponent(additionalCost) {
+    if (!additionalCost.additionalCostId) {
+      return false;
+    }
+    additionalCost.isTaxComponent = false;
+    if (
+      this.additionalCostTypes[additionalCost.additionalCostId].componentType
+    ) {
+      additionalCost.isTaxComponent = !(
+        this.additionalCostTypes[additionalCost.additionalCostId].componentType
+          .id === COMPONENT_TYPE_IDS.PRODUCT_COMPONENT
+      );
+      if (additionalCost.isTaxComponent) {
+        // console.log("Tax:" + additionalCost.additionalCost.name)
+      } else {
+        additionalCost.isTaxComponent = false;
+      }
+      return (
+        this.additionalCostTypes[additionalCost.additionalCostId].componentType
+          .id === COMPONENT_TYPE_IDS.PRODUCT_COMPONENT
+      );
+    }
+
+    return null;
+  }
+
+  /**
+   * Sum the Amount field of all products.
+   */
+  sumProductAmounts(products) {
+    let result = 0;
+    for (let i = 0; i < products.length; i++) {
+      result = result + Number(products[i].amount);
+    }
+    return result;
+  }
+
+  /**
+   * Sum the amounts of all additional costs that are NOT tax component additional costs.
+   */
+  sumProductComponentAdditionalCostAmounts(additionalCostList) {
+    let result = 0;
+    if (!additionalCostList.length) {
+      return;
+    }
+    for (let i = 0; i < additionalCostList.length; i++) {
+      if (!additionalCostList[i].isDeleted) {
+        if (
+          this.isProductComponent(additionalCostList[i]) ||
+          additionalCostList[i].costTypeId !== COST_TYPE_IDS.PERCENT
+        ) {
+          result = result + additionalCostList[i].totalAmount;
+        }
+      }
+    }
+    return result;
+  }
+
+  getRequestOffers(sellers) {
+    let requestOffers = [];
+    for (let i = 0; i < sellers.length; i++) {
+      if (sellers[i]?.requestOffers) {
+        for (let j = 0; j < sellers[i]?.requestOffers.length; j++) {
+          requestOffers.push(sellers[i]?.requestOffers[j]);
+        }
+      }
+    }
+    return requestOffers;
+  }
+
+  checkRequiredFields(): string {
+    let locationBasedCostsRequired = [];
+    for (let i = 0; i < this.locationBasedCosts.length; i++) {
+      if (
+        !this.locationBasedCosts[i].additionalCostId &&
+        !this.locationBasedCosts[i].isDeleted
+      ) {
+        locationBasedCostsRequired.push('Cost name is required!');
+      }
+    }
+    let locationBasedCostsRequiredString = '';
+    for (let i = 0; i < locationBasedCostsRequired.length; i++) {
+      locationBasedCostsRequiredString += locationBasedCostsRequired[i] + ',';
+    }
+    if (
+      locationBasedCostsRequiredString[
+        locationBasedCostsRequiredString.length - 1
+      ] == ','
+    ) {
+      locationBasedCostsRequiredString = locationBasedCostsRequiredString.substring(
+        0,
+        locationBasedCostsRequiredString.length - 1
+      );
+    }
+    return locationBasedCostsRequiredString;
+  }
+
   addNewLocationCost() {
     if (this.locationCosts.length === 0)
       this.toastr.warning('No location specific additional cost is available.');
@@ -348,6 +683,7 @@ export class ApplicablecostpopupComponent implements OnInit {
       } as AdditionalCostViewModel;
       this.locationBasedCosts.push(additionalCost);
     }
+    this.enableSave = true;
   }
 
   removeLocationCost(key: number) {
@@ -356,6 +692,7 @@ export class ApplicablecostpopupComponent implements OnInit {
     } else {
       this.locationBasedCosts.splice(key, 1);
     }
+    this.recalculatePercentAdditionalCosts(this.locationBasedCosts);
     this.enableSave = true;
   }
 
