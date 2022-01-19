8a07e5888ea7e497227b5f0666aa06493c3c1778
Suresh <suresh.r@Inatech.com>
2022-01-19 16:34:26 +0530

    37624 - Selection/de-selection does not work correctly

--- ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/spot-negotiation.component.ts
+++ ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/spot-negotiation.component.ts
@@ -52,6 +52,7 @@ export class SpotNegotiationComponent implements OnInit, OnDestroy {
   CurrentProductLength: any;
   CurrentLocationprduct: any[];
   currentRequestData: any[];
+  allRequest: any[];
 
   constructor(
     private http: HttpClient,
@@ -111,12 +112,15 @@ export class SpotNegotiationComponent implements OnInit, OnDestroy {
   getLocationRowsWithPriceDetails(rowsArray, priceDetailsArray) {
     this.store.subscribe(({ spotNegotiation, ...props }) => {
       this.currentRequestData = spotNegotiation.locations;
+      this.allRequest = spotNegotiation.requests;
     });
-
     rowsArray.forEach((row, index) => {
-      let currentLocProd = this.currentRequestData.filter(
-        row1 => row1.locationId == row.locationId
+      let rowrelatedrequest = this.allRequest.filter(
+        row1 => row1.id == row.requestId
       );
+        let currentLocProd = rowrelatedrequest[0]["requestLocations"].filter(
+          row1 => row1.locationId == row.locationId
+        );
       if (currentLocProd.length != 0) {
         let currentLocProdCount = currentLocProd[0].requestProducts.length;
         for (let index = 0; index < currentLocProdCount; index++) {
