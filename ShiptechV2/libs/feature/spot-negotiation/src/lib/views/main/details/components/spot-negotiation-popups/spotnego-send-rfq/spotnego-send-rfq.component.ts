da22577855be3d98bbeb1f614d37591287f51e68
Prabaharan <prabaharan.p@Inatech.com>
2022-01-20 15:29:46 +0530

    #37950: select disabled checkbox,#37995: Negotiation - One of the request.both fixed

--- ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/spotnego-send-rfq/spotnego-send-rfq.component.ts
+++ ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/spotnego-send-rfq/spotnego-send-rfq.component.ts
@@ -30,6 +30,7 @@ export class SpotnegoSendRfqComponent implements OnInit {
   }
 
   public toggleCheckbox(checkbox: any, item: any): void {
+    debugger;
     if (checkbox.checked) {
       item.selected = true;
     } else {
@@ -43,9 +44,21 @@ export class SpotnegoSendRfqComponent implements OnInit {
 
   ngOnInit(): void {
     const locationRows = this.store.selectSnapshot<any>((state: any) => state.spotNegotiation.locationsRows);
+    const currentRequestData = this.store.selectSnapshot<any>((state: any) => state.spotNegotiation.locations);
+    let currentLocProdCount = currentRequestData[0].requestProducts.length;
     this.requests.map(request=> {
-        request.selected = locationRows.filter(row=> row.requestId === request.id && row.isSelected).length > 0
-        request.sellerSelection = request.selected;
+        request.selected = locationRows.filter(row=> row.requestId === request.id && row.isSelected).length > 0    
+        for (let index = 0; index < currentLocProdCount; index++) {
+          let indx = index + 1;
+          let val = 'checkProd' + indx;
+          let checked=locationRows.filter(row=> row.requestId === request.id &&  row[val]=== true ).length > 0
+          if(checked){
+            request.sellerSelection =  locationRows.filter(row=> row.requestId === request.id &&  row[val]=== true ).length > 0
+          }
+        }
+        if(request.selected && !request.sellerSelection){ 
+          request.sellerSelection=request.selected;
+        }
     });
   }
 }
