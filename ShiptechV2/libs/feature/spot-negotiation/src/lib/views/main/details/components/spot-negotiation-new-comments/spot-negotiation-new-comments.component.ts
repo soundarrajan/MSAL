857148c90dfa97af93b9f0dafaadd8cf51b3bac7
Avram Ioana <Ioana.Avram@24software.ro>
2022-01-31 12:00:52 +0200

    Worked on View/ Add Comments in Negotiation (34016)

--- ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-new-comments/spot-negotiation-new-comments.component.ts
+++ ShiptechV2/libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-new-comments/spot-negotiation-new-comments.component.ts
@@ -1,27 +1,32 @@
 import {
+  AfterViewInit,
   ChangeDetectorRef,
   Component,
   ElementRef,
   OnInit,
   ViewChild
 } from '@angular/core';
+import { throwToolbarMixedModesError } from '@angular/material/toolbar';
 import { Store } from '@ngxs/store';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-spot-negotiation-new-comments',
   templateUrl: './spot-negotiation-new-comments.component.html',
   styleUrls: ['./spot-negotiation-new-comments.component.css']
 })
-export class SpotNegotiationNewCommentsComponent implements OnInit {
-  @ViewChild('comment1') commentBox1: ElementRef;
-  @ViewChild('comment2') commentBox2: ElementRef;
-  @ViewChild('comment3') commentBox3: ElementRef;
-  @ViewChild('comment4') commentBox4: ElementRef;
+export class SpotNegotiationNewCommentsComponent
+  implements OnInit, AfterViewInit {
+  @ViewChild('generalComment') generalCommentBox: ElementRef;
+  @ViewChild('performanceComment') performanceCommentBox: ElementRef;
+  @ViewChild('supplyComment') supplyCommentBox: ElementRef;
+  @ViewChild('vesselAndAgentComment') vesselAndAgentCommentBox: ElementRef;
 
-  currentRequestSmallInfo: any = null;
-  editableComments1: boolean = true;
-  editableComments2: boolean = true;
-  editableComments4: boolean = true;
+  requestInfo: any = null;
+  editableGeneralComment: boolean = false;
+  editablePerformanceComment: boolean = false;
+  editableSupplyComment: boolean = false;
+  editableVesselAndAgentComment: boolean = false;
   showEditIcon: boolean = false;
 
   notYet: string = ' - ';
@@ -29,29 +34,60 @@ export class SpotNegotiationNewCommentsComponent implements OnInit {
   constructor(private store: Store, public changeDetector: ChangeDetectorRef) {
     this.store.subscribe(({ spotNegotiation }) => {
       if (spotNegotiation.currentRequestSmallInfo) {
-        this.currentRequestSmallInfo = spotNegotiation.currentRequestSmallInfo;
+        this.requestInfo = _.cloneDeep(spotNegotiation.currentRequestSmallInfo);
+        // this.requestInfo.negoGeneralComments = 'General comments';
+        // this.requestInfo.negoPerformanceComments = 'Performance comments';
+        // this.requestInfo.negoSupplierComments = 'Supply comments';
+        // this.requestInfo.negoVesselAgentComments = 'Vessel and Agent comments';
+
+        this.editableGeneralComment =
+          this.requestInfo?.negoGeneralComments?.length > 0 ? false : true;
+        this.editablePerformanceComment =
+          this.requestInfo?.negoPerformanceComments?.length > 0 ? false : true;
+        this.editableSupplyComment =
+          this.requestInfo?.negoSupplierComments?.length > 0 ? false : true;
+        this.editableVesselAndAgentComment =
+          this.requestInfo?.negoVesselAgentComments?.length > 0 ? false : true;
+
+        console.log(this.requestInfo);
       }
     });
   }
 
   ngOnInit(): void {}
+  ngAfterViewInit() {}
 
-  editComments1(): void {
-    this.editableComments1 = false;
-    this.commentBox1.nativeElement.focus();
+  editNegotiationGeneralComment(): void {
+    this.editableGeneralComment = true;
+    this.moveCursorToEnd(this.generalCommentBox.nativeElement);
   }
 
-  editComments2(): void {
-    this.editableComments2 = false;
-    this.commentBox2.nativeElement.focus();
+  editPerformanceComment(): void {
+    this.editablePerformanceComment = true;
+    this.moveCursorToEnd(this.performanceCommentBox.nativeElement);
   }
 
-  editComments3(): void {
-    this.commentBox3.nativeElement.focus();
+  editSupplyComments(): void {
+    this.editableSupplyComment = true;
+    this.moveCursorToEnd(this.supplyCommentBox.nativeElement);
   }
 
-  editComments4(): void {
-    this.editableComments4 = false;
-    this.commentBox4.nativeElement.focus();
+  editvesselAndAgentCommentBox(): void {
+    this.editableVesselAndAgentComment = true;
+    this.moveCursorToEnd(this.vesselAndAgentCommentBox.nativeElement);
+  }
+
+  moveCursorToEnd(element) {
+    var len = element.value.length;
+    if (element.setSelectionRange) {
+      element.focus();
+      element.setSelectionRange(len, len);
+    } else if (element.createTextRange) {
+      var t = element.createTextRange();
+      t.collapse(true);
+      t.moveEnd('character', len);
+      t.moveStart('character', len);
+      t.select();
+    }
   }
 }
