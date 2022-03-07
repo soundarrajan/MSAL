04b6dc3dd1c8c6b45fe5fbb9c2698c9fdab07f91
Edwin Francis <edwin.f@Inatech.com>
2022-03-07 17:08:48 +0530

    Market price history popup updates. #38057

--- libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/marketpricehistorypopup/marketpricehistorypopup.component.ts
+++ libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/marketpricehistorypopup/marketpricehistorypopup.component.ts
@@ -11,43 +11,67 @@ import moment from 'moment';
   styleUrls: ['./marketpricehistorypopup.component.css']
 })
 export class MarketpricehistorypopupComponent implements OnInit {
-  public ProductId : number;
-  public LocationId : number;
-  public RequestId : number;
+  public ProductId: number;
+  public LocationId: number;
+  public RequestId: number;
   public tabledata = [];
-  public priceHistoryData : any = {};
+  public priceHistoryData: any = {};
   Highcharts: typeof Highcharts = Highcharts;
 
   chartOptions: Highcharts.Options = {
     chart: {
-      height: 600,
-      width: 800,
+      height: 500,
+      width: 800
     },
     title: {
-      text: ""
+      text: ''
     },
-    xAxis:{
+    xAxis: {
       lineWidth: 1,
-      lineColor: '#364150',  
-      title:{
-        text:""
-      }, 
-      categories:['20/5','21/05','24/05','25/05','26/05','27/05','02/06','03/06','05/06','07/06','08/06']
+      lineColor: '#364150',
+      title: {
+        text: ''
+      },
+      categories: [
+        '20/5',
+        '21/05',
+        '24/05',
+        '25/05',
+        '26/05',
+        '27/05',
+        '02/06',
+        '03/06',
+        '05/06',
+        '07/06',
+        '08/06'
+      ]
     },
-    yAxis: {    
-      gridLineWidth: 0,  
+    yAxis: {
+      gridLineWidth: 0,
       lineWidth: 1,
-      lineColor: '#364150',    
-        title:{
-          text:""
-        },
-        tickPixelInterval: 2 
+      lineColor: '#364150',
+      title: {
+        text: ''
+      },
+      tickPixelInterval: 2
     },
     series: [
       {
-        showInLegend: false,             
-        type: "line",
-        data: [476.00,473.00,482.00,485.00,485.00,487.00,492.00,496.00,502.00,506.00,518.00]
+        showInLegend: false,
+        type: 'line',
+        data: [
+          476.0,
+          473.0,
+          482.0,
+          485.0,
+          485.0,
+          487.0,
+          492.0,
+          496.0,
+          502.0,
+          506.0,
+          518.0
+        ]
       }
     ],
     credits: {
@@ -55,61 +79,63 @@ export class MarketpricehistorypopupComponent implements OnInit {
     }
   };
 
-  ngOnInit(): void {
-  }
-  constructor(public format: TenantFormattingService, private _spotNegotiationService: SpotNegotiationService, public dialogRef: MatDialogRef<MarketpricehistorypopupComponent>,    @Inject(MAT_DIALOG_DATA) public data: any) {
-     let payload = {LocationId : this.data.LocationId, ProductId : this.data.ProductId, RequestId: this.data.RequestId };
-     const response = this._spotNegotiationService.getMarketPriceHistory(payload);
-     response.subscribe((res: any) => {
-      if(res?.message == 'Unauthorized'){
+  ngOnInit(): void {}
+  constructor(
+    public format: TenantFormattingService,
+    private _spotNegotiationService: SpotNegotiationService,
+    public dialogRef: MatDialogRef<MarketpricehistorypopupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    let payload = {
+      LocationId: this.data.LocationId,
+      ProductId: this.data.ProductId,
+      RequestId: this.data.RequestId
+    };
+    const response = this._spotNegotiationService.getMarketPriceHistory(
+      payload
+    );
+    response.subscribe((res: any) => {
+      if (res?.message == 'Unauthorized') {
         return;
       }
       // this.priceHistoryData =  {date : res.marketPriceHistory.map(item => item.date), price : res.marketPriceHistory.map(item => item.price)};
-         res.marketPriceHistory.forEach(item=>{
-        this.tabledata.push({price:item.price,date:this.formatDate(item.date)})
-        })
-     });
-     
-   }
-   
-  closeDialog() {
-      this.dialogRef.close();
-    
-    } 
-    
-    formatDate(date?: any) {
+      res.marketPriceHistory.forEach(item => {
+        this.tabledata.push({
+          price: item.price,
+          date: this.formatDate(item.date)
+        });
+      });
+    });
+  }
 
-      if (date) {
-  
-        let currentFormat = this.format.dateFormat;
-  
-        let hasDayOfWeek;
-  
-        if (currentFormat.startsWith('DDD ')) {
-  
-          hasDayOfWeek = true;
-  
-          currentFormat = currentFormat.split('DDD ')[1];
-  
-        }
-  
-        currentFormat = currentFormat.replace(/d/g, 'D');
-  
-        currentFormat = currentFormat.replace(/y/g, 'Y');
-  
-        const elem = moment(date, 'YYYY-MM-DDTHH:mm:ss');
-  
-        let formattedDate = moment(elem).format(currentFormat);
-  
-        if (hasDayOfWeek) {
-  
-          formattedDate = `${moment(date).format('ddd')} ${formattedDate}`;
-  
-        }
-  
-        return formattedDate;
-  
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  formatDate(date?: any) {
+    if (date) {
+      let currentFormat = this.format.dateFormat;
+      let hasDayOfWeek;
+
+      if (currentFormat.startsWith('DDD ')) {
+        hasDayOfWeek = true;
+        currentFormat = currentFormat.split('DDD ')[1];
       }
-  
+      if (currentFormat.endsWith('HH:mm')) {
+        currentFormat = currentFormat.split('HH:mm')[0];
+      }
+
+      currentFormat = currentFormat.replace(/d/g, 'D');
+      currentFormat = currentFormat.replace(/y/g, 'Y');
+
+      const elem = moment(date, 'YYYY-MM-DDTHH:mm:ss');
+      let formattedDate = moment(elem).format(currentFormat);
+
+      if (hasDayOfWeek) {
+        formattedDate = `${moment(date).format('ddd')} ${formattedDate}`;
+      }
+
+      return formattedDate;
     }
+  }
 }
