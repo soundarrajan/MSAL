57403cd7c35e0cf5cbbdd7543d5a9e08dd9be822
Rinku <rinku.g@inatech.com>
2022-05-04 18:21:11 +0530

    #39834 fixed

--- libs/feature/spot-negotiation/src/lib/core/ag-grid/shiptech-custom-header-group.ts
+++ libs/feature/spot-negotiation/src/lib/core/ag-grid/shiptech-custom-header-group.ts
@@ -208,7 +208,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
                 : 'Pricing published on: ') +
               (this.closureDate == 'Invalid date' ? '--' : this.closureDate)
             "
-            matTooltipClass="outdated-tooltip"
+            matTooltipClass="lightTooltip"
           >
             <div class="title">
               <span
