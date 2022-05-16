163a092226bf3c8f6a624c3dbd0524f0b329e983
Nagarajan <nagarajan.m@Inatech.com>
2022-05-16 12:21:37 +0530

    #40339 - updated code for Trailing  of Zeros

--- libs/feature/spot-negotiation/src/lib/core/ag-grid/shiptech-custom-header-group.ts
+++ libs/feature/spot-negotiation/src/lib/core/ag-grid/shiptech-custom-header-group.ts
@@ -747,7 +747,7 @@ export class ShiptechCustomHeaderGroup {
 
     let num = plainNumber.split('.', 2);
     //Live Price to follow precision set at tenant. Ignore the precision, if the decimal values are only 0s
-    if (plainNumber == num && type == 'livePrice') {
+    if (plainNumber == num) {
       this.priceFormat = '';
     } else {
       this.priceFormat =
