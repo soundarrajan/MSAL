588cb40f6c23db7fac50f2457f215d35238d5491
Prabaharan <prabaharan.p@Inatech.com>
2022-06-03 08:15:10 +0000

    Old Angular changes--Upgrade the js file
    Workitems:
    #40429 Old Angular changes
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C34298

--- ShipTech10/app/vendors/ui-bootstrap-tpls.js
+++ ShipTech10/app/vendors/ui-bootstrap-tpls.js
@@ -7324,8 +7324,8 @@ angular.module("uib/template/typeahead/typeahead-match.html", []).run(["$templat
   $templateCache.put("uib/template/typeahead/typeahead-match.html",
     "<a href\n" +
     "   tabindex=\"-1\"\n" +
-    "   ng-bind-html=\"match.label \"\n" +
-    "   ng-attr-title=\"{{match.label | decodeReadOnly}}\"></a>\n" +
+    "   ng-bind-html=\"match.label | uibTypeaheadHighlight:query\"\n" +
+    "   ng-attr-title=\"{{match.label}}\"></a>\n" +
     "");
 }]);
 
