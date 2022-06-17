21292a8d2e82aa87231bda3e19a2fc286c7b1451
Rinku <rinku.g@inatech.com>
2022-06-17 12:19:53 +0530

    #39823 fixed Spot Negotiation - Product header fields UI issues

--- ShiptechV2/libs/feature/spot-negotiation/src/lib/core/ag-grid/shiptech-custom-header-group.ts
+++ ShiptechV2/libs/feature/spot-negotiation/src/lib/core/ag-grid/shiptech-custom-header-group.ts
@@ -212,7 +212,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
                 : 'Pricing published on: ') +
               (this.closureDate == 'Invalid date' ? '--' : this.closureDate)
             "
-            matTooltipClass="outdated-tooltip"
+            
           >
             <div class="title">
               <span
