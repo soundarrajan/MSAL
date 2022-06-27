d6ad921bfba4b7b1f7cb844079ddac42257ac3bf
Nagarajan <nagarajan.m@Inatech.com>
2022-06-27 19:36:50 +0530

    #41040 - updated code to call the new end point to get the counter parties list

--- ShiptechV2/libs/feature/spot-negotiation/src/lib/services/api/spot-negotiation-api.ts
+++ ShiptechV2/libs/feature/spot-negotiation/src/lib/services/api/spot-negotiation-api.ts
@@ -34,7 +34,7 @@ export const SpotNegotiationApiPaths = {
   // tenantConfiguration: `api/admin/tenantConfiguration/get`,
   tenantConfiguration: `Groups/getTenantConfiguration`,
   staticLists: `api/infrastructure/static/lists`,
-  counterpartyLists: `api/masters/counterparties/listbyTypes`,
+  counterpartyLists: `counterparty/getCounterpartyList`,
   addCounterparties: `groups/addSellers`,
   addRequesttoGroup: `groups/linkRequest`,
   saveTargetPrice: `Groups/saveTargetPrice`,
@@ -262,8 +262,8 @@ export class SpotNegotiationApi implements ISpotNegotiationApiService {
   getCounterpartyList(payload: any): Observable<any> {
     return this.http
       .post<any>(
-        `${this._masterApiUrl}/${SpotNegotiationApiPaths.counterpartyLists}`,
-        { Payload: payload }
+        `${this._negotiationApiUrl}/${SpotNegotiationApiPaths.counterpartyLists}`,
+         payload 
       )
       .pipe(
         map((body: any) => body),
