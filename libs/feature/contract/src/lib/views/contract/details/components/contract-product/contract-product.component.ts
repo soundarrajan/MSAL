c7655844f76d419dd64764ed5fa7eaa1d296299d
S. R. Sakthi Saileshram <sakthi.s@Inatech.com>
2022-08-10 11:56:58 +0000

    Merged PR 18807: #41643 unknown error fix in contract page
    #41643 unknown error fix in contract page
    
    Related work items: #41643

--- libs/feature/contract/src/lib/views/contract/details/components/contract-product/contract-product.component.ts
+++ libs/feature/contract/src/lib/views/contract/details/components/contract-product/contract-product.component.ts
@@ -1004,6 +1004,7 @@ export class ContractProduct extends DeliveryAutocompleteComponent
       allowedLocations: [],
       priceUom: this.generalTenantSettings.tenantFormats.uom,
       currency: this.generalTenantSettings.tenantFormats.currency
+      ,pricePrecision: this.tenantService.pricePrecision
     };
     if (this.formValues) {
       if (!this.formValues.products) {
