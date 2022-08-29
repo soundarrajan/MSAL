32cf136c7091ff58fdc751cb950d3a585aded5cc
Prathiban <prathiban.t@inatech.com>
2022-08-29 14:48:30 +0530

    42262 - NEG: Req change popup issue fix

--- libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/spotnego-otherdetails2/spotnego-otherdetails2.component.ts
+++ libs/feature/spot-negotiation/src/lib/views/main/details/components/spot-negotiation-popups/spotnego-otherdetails2/spotnego-otherdetails2.component.ts
@@ -540,12 +540,14 @@ export class SpotnegoOtherdetails2Component implements OnInit {
               reqProd = ele.requestProducts.filter(
                 item => item.id === reqOff.requestProductId
               );
-              otherDetailsPayload = this.ConstructOtherDetailsPayload(
-                reqOff,
-                etaDate,
-                reqProd
-              );
-              this.otherDetailsItems.push(otherDetailsPayload[0]);
+              if (reqProd && reqProd.length > 0) {
+                otherDetailsPayload = this.ConstructOtherDetailsPayload(
+                  reqOff,
+                  etaDate,
+                  reqProd
+                );
+                this.otherDetailsItems.push(otherDetailsPayload[0]);
+              }
             }
           });
         }
@@ -592,7 +594,7 @@ export class SpotnegoOtherdetails2Component implements OnInit {
           ? moment(etaDate).format(this.dateFormat_rel_SupplyDate)
           : '',
         product:
-        (reqProd && reqProd[0] && requestOffers.quotedProductId == reqProd[0]?.productId)
+        requestOffers.quotedProductId == reqProd[0].productId
             ? ''
             : {
                 id: requestOffers.quotedProductId,
