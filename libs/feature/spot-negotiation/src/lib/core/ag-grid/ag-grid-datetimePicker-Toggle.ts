2578d02152b7dc5e2f2cb219a96746d38f9882be
Prabaharan <prabaharan.p@Inatech.com>
2022-09-06 21:17:45 +0530

    #42587: Negotiation screen-Quote by date format should display based on the tenant settings.(implemented tentant format

--- libs/feature/spot-negotiation/src/lib/core/ag-grid/ag-grid-datetimePicker-Toggle.ts
+++ libs/feature/spot-negotiation/src/lib/core/ag-grid/ag-grid-datetimePicker-Toggle.ts
@@ -6,11 +6,15 @@ import {
   ElementRef,
   Output,
   EventEmitter,
-  Input
+  Input,
+  Injectable,
+  InjectionToken,
+  Optional,
+  Inject
 } from '@angular/core';
 import { ICellEditorAngularComp } from 'ag-grid-angular';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
-import _moment from 'moment';
+import _moment, { Moment, MomentFormatSpecification, MomentInput } from 'moment';
 import { default as _rollupMoment } from 'moment';
 const moment = _rollupMoment || _moment;
 import { MatMenuTrigger } from '@angular/material/menu';
@@ -19,6 +23,334 @@ import { SpotNegotiationService } from '../../services/spot-negotiation.service'
 import { Store } from '@ngxs/store';
 import { SetQuoteDateAndTimeZoneId } from '../../store/actions/request-group-actions';
 import { TenantFormattingService } from '@shiptech/core/services/formatting/tenant-formatting.service';
+import { NgxMatDateAdapter, NgxMatDateFormats, NGX_MAT_DATE_FORMATS } from '@angular-material-components/datetime-picker';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+const CUSTOM_DATE_FORMATS: NgxMatDateFormats = {
+  parse: {
+    dateInput: 'YYYY-MM-DD HH:mm'
+  },
+  display: {
+    dateInput: 'YYYY-MM-DD HH:mm',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY'
+  }
+};
+
+export const PICK_FORMATS = {
+  display: {
+    dateInput: 'DD MMM YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY'
+  },
+  parse: {
+    dateInput: 'DD MMM YYYY'
+  }
+};
+@Injectable()
+export class CustomDateAdapter extends MomentDateAdapter {
+  public format(value: moment.Moment, displayFormat: string): string {
+    if (value === null || value === undefined) return '';
+    let currentFormat = PICK_FORMATS.display.dateInput;
+    let hasDayOfWeek;
+    if (currentFormat.startsWith('DDD ')) {
+      hasDayOfWeek = true;
+      currentFormat = currentFormat.split('DDD ')[1];
+    }
+    currentFormat = currentFormat.replace(/d/g, 'D');
+    currentFormat = currentFormat.replace(/y/g, 'Y');
+    currentFormat = currentFormat.split(' HH:mm')[0];
+    let formattedDate = moment.utc(value).format(currentFormat);
+    if (hasDayOfWeek) {
+      formattedDate = `${moment.utc(value).format('ddd')} ${formattedDate}`;
+    }
+    return formattedDate;
+  }
+
+  parse(value) {
+    // We have no way using the native JS Date to set the parse format or locale, so we ignore these
+    // parameters.
+    let currentFormat = PICK_FORMATS.display.dateInput;
+    let hasDayOfWeek;
+    if (currentFormat.startsWith('DDD ')) {
+      hasDayOfWeek = true;
+      currentFormat = currentFormat.split('DDD ')[1];
+    }
+    currentFormat = currentFormat.replace(/d/g, 'D');
+    currentFormat = currentFormat.replace(/y/g, 'Y');
+    currentFormat = currentFormat.split(' HH:mm')[0];
+    let elem = moment.utc(value, currentFormat);
+    return value ? elem : null;
+  }
+}
+export interface NgxMatMomentDateAdapterOptions {
+  strict?: boolean;
+
+  useUtc?: boolean;
+}
+export const MAT_MOMENT_DATE_ADAPTER_OPTIONS_1 = new InjectionToken<
+  NgxMatMomentDateAdapterOptions
+>('MAT_MOMENT_DATE_ADAPTER_OPTIONS_1', {
+  providedIn: 'root',
+  factory: MAT_MOMENT_DATE_ADAPTER_OPTIONS_FACTORY
+});
+
+export function MAT_MOMENT_DATE_ADAPTER_OPTIONS_FACTORY(): NgxMatMomentDateAdapterOptions {
+  return {
+    useUtc: false
+  };
+}
+
+function range<T>(length: number, valueFunction: (index: number) => T): T[] {
+  const valuesArray = Array(length);
+  for (let i = 0; i < length; i++) {
+    valuesArray[i] = valueFunction(i);
+  }
+  return valuesArray;
+}
+
+@Injectable()
+export class CustomNgxDatetimeAdapter extends NgxMatDateAdapter<Moment> {
+  private _localeData: {
+    firstDayOfWeek: number;
+    longMonths: string[];
+    shortMonths: string[];
+    dates: string[];
+    longDaysOfWeek: string[];
+    shortDaysOfWeek: string[];
+    narrowDaysOfWeek: string[];
+  };
+
+  constructor(
+    @Optional() @Inject(MAT_DATE_LOCALE) dateLocale: string,
+    @Optional()
+    @Inject(MAT_MOMENT_DATE_ADAPTER_OPTIONS_1)
+    private _options?: NgxMatMomentDateAdapterOptions
+  ) {
+    super();
+    this.setLocale(dateLocale || moment.locale());
+  }
+
+  setLocale(locale: string) {
+    super.setLocale(locale);
+
+    const momentLocaleData = moment.localeData(locale);
+    this._localeData = {
+      firstDayOfWeek: momentLocaleData.firstDayOfWeek(),
+      longMonths: momentLocaleData.months(),
+      shortMonths: momentLocaleData.monthsShort(),
+      dates: range(31, i => this.createDate(2017, 0, i + 1).format('D')),
+      longDaysOfWeek: momentLocaleData.weekdays(),
+      shortDaysOfWeek: momentLocaleData.weekdaysShort(),
+      narrowDaysOfWeek: momentLocaleData.weekdaysMin()
+    };
+  }
+
+  getYear(date: Moment): number {
+    return this.clone(date).year();
+  }
+
+  getMonth(date: Moment): number {
+    return this.clone(date).month();
+  }
+
+  getDate(date: Moment): number {
+    return this.clone(date).date();
+  }
+
+  getDayOfWeek(date: Moment): number {
+    return this.clone(date).day();
+  }
+
+  getMonthNames(style: 'long' | 'short' | 'narrow'): string[] {
+    // Moment.js doesn't support narrow month names, so we just use short if narrow is requested.
+    return style === 'long'
+      ? this._localeData.longMonths
+      : this._localeData.shortMonths;
+  }
+
+  getDateNames(): string[] {
+    return this._localeData.dates;
+  }
+
+  getDayOfWeekNames(style: 'long' | 'short' | 'narrow'): string[] {
+    if (style === 'long') {
+      return this._localeData.longDaysOfWeek;
+    }
+    if (style === 'short') {
+      return this._localeData.shortDaysOfWeek;
+    }
+    return this._localeData.narrowDaysOfWeek;
+  }
+
+  getYearName(date: Moment): string {
+    return this.clone(date).format('YYYY');
+  }
+
+  getFirstDayOfWeek(): number {
+    return this._localeData.firstDayOfWeek;
+  }
+
+  getNumDaysInMonth(date: Moment): number {
+    return this.clone(date).daysInMonth();
+  }
+
+  clone(date: Moment): Moment {
+    return date.clone().locale(this.locale);
+  }
+
+  createDate(year: number, month: number, date: number): Moment {
+    if (month < 0 || month > 11) {
+      throw Error(
+        `Invalid month index "${month}". Month index has to be between 0 and 11.`
+      );
+    }
+
+    if (date < 1) {
+      throw Error(`Invalid date "${date}". Date has to be greater than 0.`);
+    }
+
+    const result = this._createMoment({ year, month, date }).locale(
+      this.locale
+    );
+    if (!result.isValid()) {
+      throw Error(`Invalid date "${date}" for month with index "${month}".`);
+    }
+
+    return result;
+  }
+
+  today(): Moment {
+    // @ts-ignore
+    return this._createMoment().locale(this.locale);
+  }
+
+  parse(value: any, parseFormat: string | string[]): Moment | null {
+    let currentFormat = PICK_FORMATS.display.dateInput;
+    let hasDayOfWeek;
+    if (currentFormat.startsWith('DDD ')) {
+      hasDayOfWeek = true;
+      currentFormat = currentFormat.split('DDD ')[1];
+    }
+    currentFormat = currentFormat.replace(/d/g, 'D');
+    currentFormat = currentFormat.replace(/y/g, 'Y');
+    const elem = moment(value, currentFormat);
+    const isValid = this.isValid(elem);
+    return this.isValid(elem) ? elem : null;
+  }
+
+  format(date: Moment, displayFormat: string): string {
+    date = this.clone(date);
+    if (!this.isValid(date)) {
+      throw Error('MomentDateAdapter: Cannot format invalid date.');
+    }
+    let currentFormat = CUSTOM_DATE_FORMATS.display.dateInput;
+    let hasDayOfWeek;
+    if (currentFormat.startsWith('DDD ')) {
+      hasDayOfWeek = true;
+      currentFormat = currentFormat.split('DDD ')[1];
+    }
+    currentFormat = currentFormat.replace(/d/g, 'D');
+    currentFormat = currentFormat.replace(/y/g, 'Y');
+    let formattedDate = moment(date).format(currentFormat);
+    if (hasDayOfWeek) {
+      formattedDate = `${moment(date).format('ddd')} ${formattedDate}`;
+    }
+    return formattedDate;
+  }
+
+  addCalendarYears(date: Moment, years: number): Moment {
+    return this.clone(date).add({ years });
+  }
+
+  addCalendarMonths(date: Moment, months: number): Moment {
+    return this.clone(date).add({ months });
+  }
+
+  addCalendarDays(date: Moment, days: number): Moment {
+    return this.clone(date).add({ days });
+  }
+
+  toIso8601(date: Moment): string {
+    return this.clone(date).format();
+  }
+
+  deserialize(value: any): Moment | null {
+    let date;
+    if (value instanceof Date) {
+      date = this._createMoment(value);
+    } else if (this.isDateInstance(value)) {
+      return this.clone(value);
+    }
+    if (typeof value === 'string') {
+      if (!value) {
+        return null;
+      }
+      let currentFormat = PICK_FORMATS.display.dateInput;
+      let hasDayOfWeek;
+      if (currentFormat.startsWith('DDD ')) {
+        hasDayOfWeek = true;
+        currentFormat = currentFormat.split('DDD ')[1];
+      }
+      currentFormat = currentFormat.replace(/d/g, 'D');
+      currentFormat = currentFormat.replace(/y/g, 'Y');
+      const elem = moment(value, 'YYYY-MM-DDTHH:mm:ss');
+      const newVal = moment(elem).format(currentFormat);
+      if (elem && this.isValid(elem)) {
+        return elem;
+      }
+    }
+    return super.deserialize(value);
+  }
+
+  isDateInstance(obj: any): boolean {
+    return moment.isMoment(obj);
+  }
+
+  isValid(date: Moment): boolean {
+    return this.clone(date).isValid();
+  }
+
+  invalid(): Moment {
+    return moment.invalid();
+  }
+
+  getHour(date: Moment): number {
+    const el = date.hours();
+    const elem = moment(date).utcOffset(0);
+    return date.hours();
+  }
+  getMinute(date: Moment): number {
+    return date.minutes();
+  }
+  getSecond(date: Moment): number {
+    return date.seconds();
+  }
+  setHour(date: Moment, value: number): void {
+    date.hours(value);
+  }
+  setMinute(date: Moment, value: number): void {
+    date.minutes(value);
+  }
+  setSecond(date: Moment, value: number): void {
+    date.seconds(value);
+  }
+
+  private _createMoment(
+    date: MomentInput,
+    format?: MomentFormatSpecification,
+    locale?: string
+  ): Moment {
+    const { strict, useUtc }: NgxMatMomentDateAdapterOptions =
+      this._options || {};
+
+    return useUtc
+      ? moment.utc(date, format, locale, strict)
+      : moment(date, format, locale, strict);
+  }
+}
 @Component({
   selector: 'app-date-time-toggle',
   template: `
@@ -81,7 +413,17 @@ import { TenantFormattingService } from '@shiptech/core/services/formatting/tena
       (afterPickerOpen)="timepickerOpened()"
     ></owl-date-time>
         </div>
-  `
+  `,
+  providers: [
+    { provide: DateAdapter, useClass: CustomDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: PICK_FORMATS },
+    {
+      provide: NgxMatDateAdapter,
+      useClass: CustomNgxDatetimeAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    { provide: NGX_MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMATS }
+  ]
 })
 export class AgGridDatetimePickerToggleComponent
   implements ICellEditorAngularComp, AfterViewInit {
@@ -103,7 +445,14 @@ export class AgGridDatetimePickerToggleComponent
   constructor(    
     private store: Store,
     private format: TenantFormattingService,
+    @Inject(MAT_DATE_FORMATS) private dateFormats,
+    @Inject(NGX_MAT_DATE_FORMATS) private dateTimeFormats,
     private spotNegotiationService: SpotNegotiationService) {
+      this.dateFormats.display.dateInput = this.format.dateFormat;
+      this.dateFormats.parse.dateInput = this.format.dateFormat;
+      this.dateTimeFormats.display.dateInput = this.format.dateFormat;
+      CUSTOM_DATE_FORMATS.display.dateInput = this.format.dateFormat;
+      PICK_FORMATS.display.dateInput = this.format.dateFormat;
     //this.appContext = appContext || AppContext.instance;
   }
   @ViewChild('dateInputFlde', { read: ViewContainerRef }) public input;
@@ -139,7 +488,7 @@ export class AgGridDatetimePickerToggleComponent
       (event.value.getMinutes() < 10 ? '0' : '') + event.value.getMinutes();
     this.timeValue = h + ':' + m;
     //this.timeValue = "10:15";
-    this.initialDate=new FormControl(moment(this.spotNegotiationService.QuoteByDate));
+    this.initialDate=new FormControl(moment(this.spotNegotiationService.QuoteByDate),);
     this.initialDate = this.getValue();
     this.updateQuoteByGroup(this.initialDate);
   }
