05a31c86244e42a6aa097f1d48158fd0dec92ba5
Prathiban T <prathiban.t@inatech.onmicrosoft.com>
2022-09-15 04:46:27 +0000

    20220915.1 - Merge 11.0-WF to 11.1
    git-tfs-id: [https://dev.azure.com/inatech]$/Shiptech10/Dev/Frontend;C34937

--- ShipTech10/app/app-masters/controllers.js
+++ ShipTech10/app/app-masters/controllers.js
@@ -145,7 +145,7 @@
         $scope.reloadPage = false;
 
         $(document).on('click',  function (event) {
-            if ($(event.target).parent()[0].nodeName != 'LI' && $(event.target).parents('.st-main-content-menu').length && ($(event.target).hasClass('btn') || $(event.target).hasClass('ladda-label'))) {
+            if ($(event.target).parent()[0]?.nodeName != 'LI' && $(event.target).parents('.st-main-content-menu').length && ($(event.target).hasClass('btn') || $(event.target).hasClass('ladda-label'))) {
                 window.actionLevel = event.target.outerText.trim();
                 if (event.target.outerText == 'Save') {
                     let length = window.location.href.split('/#/')[1].split('/').length - 1;
@@ -1047,7 +1047,7 @@
                 console.log($rootScope.listOfVesselTypes);
                 _.forEach(types, function(type) {
                     $scope.formValues[dataSrcs[type]] = [];
-                    for (let i = 0; i < $rootScope.tabData[type].length; i++) {
+                    for (let i = 0; i < $rootScope.tabData[type]?.length; i++) {
                         console.log($rootScope.tabData[type][i]);
                         setAllChild($rootScope.tabData[type][i], dataSrcs[type]);
                     }
