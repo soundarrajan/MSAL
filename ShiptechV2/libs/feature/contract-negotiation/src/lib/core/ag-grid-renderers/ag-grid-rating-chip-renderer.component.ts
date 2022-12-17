aab5c4760c876a097e732fe3c0b3e1d17e696087
Vinoth Kumar SV <vinothkumar.s@Inatech.com>
2022-12-17 23:23:32 +0530

    #43971 - port rating display fix

--- ShiptechV2/libs/feature/contract-negotiation/src/lib/core/ag-grid-renderers/ag-grid-rating-chip-renderer.component.ts
+++ ShiptechV2/libs/feature/contract-negotiation/src/lib/core/ag-grid-renderers/ag-grid-rating-chip-renderer.component.ts
@@ -7,20 +7,21 @@ import { SellerratingpopupComponent } from '@shiptech/core/ui/components/designs
     template: `
     <div [ngClass]="params.cellClass" *ngIf = "params.value.grating != null "
     (click)="sellerratingpopup(params.data, params.label)">
-    <div  class="truncate-125 chip">
-        <div class="m-lr-5"  *ngIf = "params.label == 'port-rating'" >
-            {{params.value.prating}}
-            <span class="star"></span>
-        </div>
-        <div class="m-lr-5"  *ngIf = "params.label == 'gen-rating'" >
-        {{params.value.grating}}
-        <span class="star"></span>
-       </div>
-        <div>{{params.value.gprice}}</div>
-    </div>
+      <div  class="truncate-125 chip" *ngIf = "params.label == 'gen-rating' && params.value.grating != null" >
+          <div class="m-lr-5"  >
+              {{params.value.grating}}
+              <span class="star"></span>
+          </div>       
+      </div>
+      <div  class="truncate-125 chip" *ngIf = "(params.label == 'port-rating' && params.value.prating != null)" >
+      <div class="m-lr-5"  >
+          {{params.value.prating}}
+          <span class="star"></span>
+      </div>       
   </div>
-  <div  class="truncate-125 chip" *ngIf = "params.value.grating == null " style="background-color: rgb(196, 196, 196) !important">
-  <div class="m-lr-5">NA</div>
+  </div>
+  <div  class="truncate-125 chip" *ngIf = "(params.value.grating == null && params.label == 'gen-rating' || params.value.prating == null && params.label == 'port-rating')" style="background-color: rgb(196, 196, 196) !important">
+    <div class="m-lr-5">NA</div>
   </div>
     `,
     styles: [
