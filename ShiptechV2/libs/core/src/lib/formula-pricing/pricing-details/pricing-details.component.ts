34abd9a58be93f08c1347f0aa4cb71afbc89eb25
Vinoth Kumar SV <vinothkumar.s@Inatech.com>
2023-02-16 13:02:10 +0530

    #41052 removed the max period list

--- ShiptechV2/libs/core/src/lib/formula-pricing/pricing-details/pricing-details.component.ts
+++ ShiptechV2/libs/core/src/lib/formula-pricing/pricing-details/pricing-details.component.ts
@@ -110,9 +110,7 @@ export class negoPricingDetailsComponent implements OnInit {
     {"id": 1, "name": "3 months"},
     {"id": 2, "name": "6 months"},
     {"id": 3, "name": "9 months"},
-    {"id": 4, "name": "12 months"},   
-    {"id": 5, "name": "15 months"},
-    {"id": 6, "name": "18 months"},      
+    {"id": 4, "name": "12 months"}    
   ];
   additionalCost: number;
   constructor(
